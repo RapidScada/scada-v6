de1d9317a0fee6c5abed1f8060fecf35496cb626
2mik <m_shir@mail.ru>
2020-06-02 19:38:12 +0300

    Create ICommContext.cs

---
+++ ScadaComm/ScadaComm/ScadaCommCommon/Drivers/ICommContext.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * Copyright 2020 Mikhail Shiryaev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Product  : Rapid SCADA
+ * Module   : ScadaCommCommon
+ * Summary  : Defines functionality to access the Communicator features
+ * 
+ * Author   : Mikhail Shiryaev
+ * Created  : 2020
+ * Modified : 2020
+ */
+
+using Scada.Comm.Config;
+using Scada.Data.Models;
+using Scada.Log;
+
+namespace Scada.Comm.Drivers
+{
+    /// <summary>
+    /// Defines functionality to access the Communicator features.
+    /// <para>Определяет функциональность для доступа к функциям Коммуникатора.</para>
+    /// </summary>
+    public interface ICommContext
+    {
+        /// <summary>
+        /// Gets the configuration database cache.
+        /// </summary>
+        BaseDataSet BaseDataSet { get; }
+
+        /// <summary>
+        /// Gets the Communicator configuration.
+        /// </summary>
+        CommConfig Config { get; }
+
+        /// <summary>
+        /// Gets the application directories.
+        /// </summary>
+        CommDirs AppDirs { get; }
+
+        /// <summary>
+        /// Gets the application log.
+        /// </summary>
+        ILog Log { get; }
+
+
+        /// <summary>
+        /// Sends the telecontrol command to the current application.
+        /// </summary>
+        void SendCommand(TeleCommand cmd);
+    }
+}
