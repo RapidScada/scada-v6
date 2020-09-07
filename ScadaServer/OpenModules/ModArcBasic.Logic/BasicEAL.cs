d3da3957450fc8bb20a2b100b7df0881e207ba12
2mik <m_shir@mail.ru>
2020-09-07 15:36:49 +0300

    Initial commit of ModArcBasic

---
+++ ScadaServer/OpenModules/ModArcBasic.Logic/BasicEAL.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scada.Server.Modules.ModArcBasic.Logic
+{
+    class BasicEAL
+    {
+    }
+}
---
+++ ScadaServer/OpenModules/ModArcBasic.Logic/ModArcBasicLogic.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * Copyright 2020 Mikhail Shiryaev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Product  : Rapid SCADA
+ * Module   : ModArcBasic
+ * Summary  : Implements the server module logic
+ * 
+ * Author   : Mikhail Shiryaev
+ * Created  : 2020
+ * Modified : 2020
+ */
+
+using Scada.Server.Archives;
+using Scada.Server.Config;
+
+namespace Scada.Server.Modules.ModArcBasic.Logic
+{
+    /// <summary>
+    /// Implements the server module logic.
+    /// <para>Реализует логику серверного модуля.</para>
+    /// </summary>
+    public class ModArcBasicLogic : ModuleLogic
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public ModArcBasicLogic(IServerContext serverContext)
+            : base(serverContext)
+        {
+        }
+
+
+        /// <summary>
+        /// Gets the module code.
+        /// </summary>
+        public override string Code
+        {
+            get
+            {
+                return "ModArcBasic";
+            }
+        }
+
+        /// <summary>
+        /// Gets the module purposes.
+        /// </summary>
+        public override ModulePurposes ModulePurposes
+        {
+            get
+            {
+                return ModulePurposes.Archive;
+            }
+        }
+
+
+        /// <summary>
+        /// Creates a new archive logic.
+        /// </summary>
+        public override ArchiveLogic CreateArchive(ArchiveConfig archiveConfig)
+        {
+            switch (archiveConfig.Kind)
+            {
+                case ArchiveKind.Current:
+                    return new BasicCAL(archiveConfig);
+                default:
+                    return null;
+            }
+        }
+    }
+}
