00f598a3b2e796d83808b1a6d4aa291e24c454a0
2mik <m_shir@mail.ru>
2020-10-28 18:41:47 +0300

    Create CnlDataPoint.cs

---
+++ ScadaData/ScadaData/Data/Models/CnlDataPoint.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * Copyright 2020 Mikhail Shiryaev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Product  : Rapid SCADA
+ * Module   : ScadaData
+ * Summary  : Represents an input channel data point
+ * 
+ * Author   : Mikhail Shiryaev
+ * Created  : 2020
+ * Modified : 2020
+ */
+
+using System;
+
+namespace Scada.Data.Models
+{
+    /// <summary>
+    /// Represents an input channel data point.
+    /// <para>Представляет точку данных входного канала.</para>
+    /// </summary>
+    public struct CnlDataPoint
+    {
+        /// <summary>
+        /// Initializes a new instance of the structure.
+        /// </summary>
+        public CnlDataPoint(int cnlNum, DateTime timestamp)
+        {
+            CnlNum = cnlNum;
+            Timestamp = timestamp;
+            Val = 0.0;
+            Stat = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the structure.
+        /// </summary>
+        public CnlDataPoint(int cnlNum, DateTime timestamp, double val, int stat)
+        {
+            CnlNum = cnlNum;
+            Timestamp = timestamp;
+            Val = val;
+            Stat = stat;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the structure.
+        /// </summary>
+        public CnlDataPoint(int cnlNum, DateTime timestamp, CnlData cnlData)
+        {
+            CnlNum = cnlNum;
+            Timestamp = timestamp;
+            Val = cnlData.Val;
+            Stat = cnlData.Stat;
+        }
+
+
+        /// <summary>
+        /// Gets the input channel number of the trend.
+        /// </summary>
+        public int CnlNum { get; }
+
+        /// <summary>
+        /// Gets or sets the timestamp.
+        /// </summary>
+        public DateTime Timestamp { get; set; }
+
+        /// <summary>
+        /// Gets or sets the channel value.
+        /// </summary>
+        public double Val { get; set; }
+
+        /// <summary>
+        /// Gets or sets the channel status.
+        /// </summary>
+        public int Stat { get; set; }
+    }
+}
