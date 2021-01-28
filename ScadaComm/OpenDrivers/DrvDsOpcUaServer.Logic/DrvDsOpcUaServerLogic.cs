207d19b8b5a9376ae66b47288a1138a49c013812
2mik <m_shir@mail.ru>
2021-01-28 14:50:49 +0300

    Initial commit of DrvDsOpcUaServer

---
+++ ScadaComm/OpenDrivers/DrvDsOpcUaServer.Logic/DrvDsOpcUaServerLogic.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scada.Comm.Drivers.DrvDsOpcUaServer.Logic
+{
+    public class DrvDsOpcUaServerLogic
+    {
+    }
+}
