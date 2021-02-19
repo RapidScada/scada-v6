e883454a6c5977044d1fedface871529a6979e41
2mik <m_shir@mail.ru>
2021-02-19 13:52:46 +0300

    ScadaComm: update copyright

--- ScadaComm/ScadaComm/ScadaCommCommon/Drivers/ILineContext.cs
+++ ScadaComm/ScadaComm/ScadaCommCommon/Drivers/ILineContext.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2021 Mikhail Shiryaev
+ * Copyright 2021 Rapid Software LLC
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
