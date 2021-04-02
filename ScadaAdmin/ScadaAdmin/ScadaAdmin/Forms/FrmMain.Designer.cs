7549e1e663bf409f3e8be1af0405bd3766cfbac1
2mik <m_shir@mail.ru>
2021-04-02 15:38:39 +0300

    ScadaAdmin: VS updated main form

--- ScadaAdmin/ScadaAdmin/ScadaAdmin/Forms/FrmMain.Designer.cs
+++ ScadaAdmin/ScadaAdmin/ScadaAdmin/Forms/FrmMain.Designer.cs
@@ -729,7 +729,7 @@
             this.wctrlMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.wctrlMain.Image = null;
             this.wctrlMain.Location = new System.Drawing.Point(0, 0);
-            this.wctrlMain.MessageFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.wctrlMain.MessageFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.wctrlMain.Name = "wctrlMain";
             this.wctrlMain.SaveReqCancel = "Cancel";
             this.wctrlMain.SaveReqCaption = "Save changes";
