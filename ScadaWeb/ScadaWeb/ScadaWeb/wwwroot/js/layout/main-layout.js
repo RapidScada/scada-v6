4c3a2c2dc84ea8433cdad71ae5baef68f7084846
2mik <m_shir@mail.ru>
2021-05-19 22:44:18 +0300

    ScadaWeb: main layout

---
+++ ScadaWeb/ScadaWeb/ScadaWeb/wwwroot/js/layout/main-layout.js
@@ -0,0 +1,29 @@
+﻿var mainLayout = {
+    // Updates the layout to fit the window.
+    updateLayout: function () {
+        let divHeader = $("#Main_divHeader");
+        let headerHeight = divHeader.length > 0 && $("body").hasClass("header-visible")
+            ? divHeader.outerHeight()
+            : 0;
+
+        let contentHeight = $(window).height() - headerHeight;
+        $("#Main_divLeftPanel").outerHeight(contentHeight);
+        $("#Main_divContent").outerHeight(contentHeight);
+        $("#Main_divNotifPanel").outerHeight(contentHeight);
+        //$(window).trigger(scada.EventTypes.UPDATE_LAYOUT);
+    },
+};
+
+$(document).ready(function () {
+    if ($("#Main_divHeader").length > 0) {
+        $("body").addClass("header-visible");
+    }
+
+    $("body").addClass("left-panel-visible");
+    mainLayout.updateLayout();
+
+    // update layout on window resize
+    $(window).resize(function () {
+        mainLayout.updateLayout();
+    });
+});
