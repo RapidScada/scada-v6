a8ea6786aeb1e325bcf934a1709fa5acd2e2efd9
2mik <m_shir@mail.ru>
2021-05-21 11:11:06 +0300

    ScadaWeb: left panel

--- ScadaWeb/ScadaWeb/ScadaWeb/wwwroot/js/layout/main-layout.js
+++ ScadaWeb/ScadaWeb/ScadaWeb/wwwroot/js/layout/main-layout.js
@@ -8,10 +8,43 @@
 
         let contentHeight = $(window).height() - headerHeight;
         $("#Main_divLeftPanel").outerHeight(contentHeight);
+        $("#Main_divTabPanel").outerWidth(contentHeight);
         $("#Main_divContent").outerHeight(contentHeight);
         $("#Main_divNotifPanel").outerHeight(contentHeight);
         //$(window).trigger(scada.EventTypes.UPDATE_LAYOUT);
     },
+
+    // Activates the selected tab and shows corresponding tool window.
+    activateTab: function (selectedTab) {
+        // highlight clicked tab
+        let tabs = selectedTab.siblings(".tab");
+        tabs.removeClass("selected");
+        selectedTab.addClass("selected");
+
+        // deactivate all tool windows
+        var toolWindows = $("#Main_divLeftPanel .tool-window");
+        toolWindows.addClass("hidden");
+
+        // activate corresponding tool window
+        var toolWindowId = selectedTab.data("tool-window");
+        $("#" + toolWindowId).removeClass("hidden");
+    },
+
+    bindEvents: function () {
+        let thisObj = this;
+
+        // update layout on window resize
+        $(window).resize(function () {
+            mainLayout.updateLayout();
+        });
+
+        // activate a clicked tab
+        $("#Main_divTabPanel .tab")
+            .off()
+            .click(function () {
+                thisObj.activateTab($(this));
+            });
+    }
 };
 
 $(document).ready(function () {
@@ -22,8 +55,6 @@ $(document).ready(function () {
     $("body").addClass("left-panel-visible");
     mainLayout.updateLayout();
 
-    // update layout on window resize
-    $(window).resize(function () {
-        mainLayout.updateLayout();
-    });
+    mainLayout.activateTab($("#Main_divMainMenuTab"));
+    mainLayout.bindEvents();
 });
