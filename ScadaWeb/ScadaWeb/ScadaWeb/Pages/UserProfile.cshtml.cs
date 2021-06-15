e648480cb3574e9feeced12b73100071276d8b0b
2mik <m_shir@mail.ru>
2021-06-15 15:28:25 +0300

    ScadaWeb: error and access denied pages

--- ScadaWeb/ScadaWeb/ScadaWeb/Pages/UserProfile.cshtml.cs
+++ ScadaWeb/ScadaWeb/ScadaWeb/Pages/UserProfile.cshtml.cs
@@ -23,10 +23,6 @@
  * Modified : 2021
  */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Scada.Web.Lang;
