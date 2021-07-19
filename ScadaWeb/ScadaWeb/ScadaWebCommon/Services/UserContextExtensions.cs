68d5e980ffd21cd789fd0d43878d22b8ddf4a4b9
2mik <m_shir@mail.ru>
2021-07-19 20:05:07 +0300

    Create UserContextExtensions.cs

---
+++ ScadaWeb/ScadaWeb/ScadaWebCommon/Services/UserContextExtensions.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Copyright 2021 Rapid Software LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Product  : Rapid SCADA
+ * Module   : ScadaWebCommon
+ * Summary  : Provides extensions to the IUserContext interface
+ * 
+ * Author   : Mikhail Shiryaev
+ * Created  : 2021
+ * Modified : 2021
+ */
+
+using System;
+
+namespace Scada.Web.Services
+{
+    /// <summary>
+    /// Provides extensions to the IUserContext interface.
+    /// <para>Предоставляет расширения интерфейса IUserContext.</para>
+    /// </summary>
+    public static class UserContextExtensions
+    {
+        /// <summary>
+        /// Converts a universal time (UTC) to the time in the user's time zone.
+        /// </summary>
+        public static DateTime ConvertTimeFromUtc(this IUserContext userContext, DateTime dateTime)
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(dateTime, userContext.TimeZone);
+        }
+
+        /// <summary>
+        /// Converts the time in the user's time zone to a universal time (UTC).
+        /// </summary>
+        public static DateTime ConvertTimeToUtc(this IUserContext userContext, DateTime dateTime)
+        {
+            return TimeZoneInfo.ConvertTimeToUtc(dateTime, userContext.TimeZone);
+        }
+    }
+}
