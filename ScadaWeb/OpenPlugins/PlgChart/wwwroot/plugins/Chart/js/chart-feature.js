e3ef464e993bf2d21cc7bb5e0ba94e3faf9914b7
2mik <m_shir@mail.ru>
2021-07-25 13:41:06 +0300

    PlgChart: chart feature script

---
+++ ScadaWeb/OpenPlugins/PlgChart/wwwroot/plugins/Chart/js/chart-feature.js
@@ -0,0 +1,9 @@
+﻿class ChartFeature extends BaseChartFeature {
+    constructor(appEnv) {
+        super(appEnv);
+    }
+
+    show(cnlNums, startDate) {
+        window.open(appEnv.rootPath + `Chart/Chart?cnlNums=${cnlNums}&startDate=${startDate}`);
+    }
+}
