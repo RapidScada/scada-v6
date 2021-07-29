db867f00d0fbb56ae26d28ed15057fe5d8371a9f
2mik <m_shir@mail.ru>
2021-07-29 15:17:39 +0300

    PlgMain: show and scroll events

--- ScadaWeb/OpenPlugins/PlgMain/wwwroot/plugins/Main/js/events.js
+++ ScadaWeb/OpenPlugins/PlgMain/wwwroot/plugins/Main/js/events.js
@@ -3,12 +3,23 @@
 // The variables below are set from Events.cshtml
 var phrases = {};
 
+var POSTPONE_SCROLL_PERIOD = 10000; // ms
+
 var filterID = 0;
 var arcWriteTime = 0;
+var scrollTimeoutID = 0;
+
+function initTooltips() {
+    new bootstrap.Tooltip(document.getElementById("spanInfoBtn"));
+};
 
 function bindEvents() {
     $(window).resize(function () {
         updateLayout();
+    });
+
+    $("#divTableWrapper").on("mousemove wheel touchstart", function () {
+        postponeScroll();
     });
 }
 
@@ -36,9 +47,9 @@ function updateEvents(callback) {
                         pluginOptions.eventCount, viewHub.viewID, filterID, function (dto) {
 
                             if (dto.ok) {
-                                filterID = dto.data.filterID;
                                 arcWriteTime = newArcWriteTime;
-                                showEvents(dto.data);
+                                showEvents(dto.data, filterID > 0);
+                                filterID = dto.data.filterID;
                             } else {
                                 showErrorBadge();
                             }
@@ -61,7 +72,7 @@ function updateEvents(callback) {
     }
 }
 
-function showEvents(data) {
+function showEvents(data, animateScroll) {
     if (data.records.length > 0) {
         let tbodyElem = $("<tbody></tbody>");
 
@@ -90,6 +101,11 @@ function showEvents(data) {
         $("#divTableWrapper tbody").remove();
         $("#divTableWrapper table").append(tbodyElem);
         $("#divTableWrapper").removeClass("hidden");
+
+        // scroll down
+        if (scrollTimeoutID <= 0) {
+            scrollDownEvents(animateScroll);
+        }
     } else {
         $("#divTableWrapper tbody").remove();
         showMessage(phrases.NoEvents);
@@ -101,9 +117,35 @@ function showMessage(text) {
     $("#divMessage").text(text).removeClass("hidden");
 }
 
+function postponeScroll() {
+    clearTimeout(scrollTimeoutID);
+
+    scrollTimeoutID = setTimeout(function () {
+        scrollDownEvents(true);
+        scrollTimeoutID = 0;
+    }, POSTPONE_SCROLL_PERIOD);
+}
+
+function scrollDownEvents(animate) {
+    let divTableWrapper = $("#divTableWrapper");
+    let scrollHeight = divTableWrapper[0].scrollHeight;
+    let newScrollTop = scrollHeight - divTableWrapper.innerHeight();
+
+    if (divTableWrapper.scrollTop() < newScrollTop) {
+        if (animate) {
+            divTableWrapper.animate({ scrollTop: newScrollTop }, "slow");
+        } else {
+            divTableWrapper.scrollTop(newScrollTop);
+        }
+    }
+}
+
 $(document).ready(function () {
+    initTooltips();
     bindEvents();
     updateLayout();
     showMessage(phrases.Loading);
     startUpdatingEvents();
+
+    $("#spanEventsByViewBtn").addClass("selected");
 });
