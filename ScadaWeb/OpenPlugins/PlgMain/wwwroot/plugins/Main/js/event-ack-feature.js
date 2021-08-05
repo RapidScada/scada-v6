3b9c5a12196c62b7a398af9229ace813a17793f2
2mik <m_shir@mail.ru>
2021-08-05 14:41:40 +0300

    PlgMain: create event ack form

--- ScadaWeb/OpenPlugins/PlgMain/wwwroot/plugins/Main/js/event-ack-feature.js
+++ ScadaWeb/OpenPlugins/PlgMain/wwwroot/plugins/Main/js/event-ack-feature.js
@@ -1,1 +1,12 @@
-﻿
+﻿class EventAckFeature extends BaseEventAckFeature {
+    constructor(appEnv) {
+        super(appEnv);
+    }
+
+    show(archiveBit, eventID, opt_callback) {
+        ModalManager.getInstance().showModal(
+            appEnv.rootPath + `Main/EventAck?archiveBit=${archiveBit}&eventID=${eventID}`,
+            new ModalOptions([ModalButton.OK, ModalButton.CANCEL]),
+            opt_callback);
+    }
+}
