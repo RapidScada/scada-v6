3a3214d579859e8f2aa5731154ac1e1e6950d7c1
2mik <m_shir@mail.ru>
2021-09-15 10:03:48 +0300

    Initial commit of ModArcBasic.Common

---
+++ ScadaServer/OpenModules/ModArcBasic.Common/BasicHAO.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Config;
+using Scada.Server.Archives;
+
+namespace Scada.Server.Modules.ModArcBasic
+{
+    /// <summary>
+    /// Represents options of a historical data archive.
+    /// <para>Представляет параметры архива исторических данных.</para>
+    /// </summary>
+    internal class BasicHAO : HistoricalArchiveOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public BasicHAO(OptionList options)
+            : base(options)
+        {
+            IsCopy = options.GetValueAsBool("IsCopy");
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the archive stores a copy of the data.
+        /// </summary>
+        public bool IsCopy { get; set; }
+    }
+}
