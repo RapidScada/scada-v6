bc0871dfe34b6c8f2dbb81e96b3aa384d4b9291c
2mik <m_shir@mail.ru>
2021-09-23 12:31:18 +0300

    Storage: add write_time column

--- Database/Storage/sql/04_app_storage.sql
+++ Database/Storage/sql/04_app_storage.sql
@@ -9,6 +9,7 @@ CREATE TABLE project.app_storage (
 	app_id int4 NOT NULL,
 	"path" varchar NOT NULL,
 	"content" varchar NULL,
+	write_time timestamp with time zone,
 	CONSTRAINT un_app_storage_app_path UNIQUE (app_id, path),
 	CONSTRAINT pk_app_storage PRIMARY KEY (file_id)
 );
