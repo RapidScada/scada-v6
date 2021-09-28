376d8376b0748d74ae5de37ba76ba934cc5fa57c
2mik <m_shir@mail.ru>
2021-09-28 18:08:10 +0300

    ModArcPostgreSql: view

---
+++ ScadaServer/OpenModules/ModArcPostgreSql.View/ModArcPostgreSqlView.cs
@@ -0,0 +1,92 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Forms;
+using Scada.Lang;
+using Scada.Server.Archives;
+using Scada.Server.Config;
+using System.Windows.Forms;
+
+namespace Scada.Server.Modules.ModArcPostgreSql.View
+{
+    /// <summary>
+    /// Implements the server module user interface.
+    /// <para>Реализует пользовательский интерфейс серверного модуля.</para>
+    /// </summary>
+    public class ModArcPostgreSqlView : ModuleView
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public ModArcPostgreSqlView()
+        {
+            CanShowProperties = true;
+        }
+
+
+        /// <summary>
+        /// Gets the module name.
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return Locale.IsRussian ? "Архив PostgreSQL" : "PostgreSQL Archive";
+            }
+        }
+
+        /// <summary>
+        /// Gets the module description.
+        /// </summary>
+        public override string Descr
+        {
+            get
+            {
+                return Locale.IsRussian ?
+                    "Модуль предоставляет архивирование данных в базу данных PostgreSQL." :
+                    "The module provides data archiving into PostgreSQL database.";
+            }
+        }
+
+
+        /// <summary>
+        /// Shows a modal dialog box for editing module properties.
+        /// </summary>
+        public override bool ShowProperties()
+        {
+            //return new FrmDir(AppDirs.ConfigDir).ShowDialog() == DialogResult.OK;
+            return false;
+        }
+
+        /// <summary>
+        /// Loads language dictionaries.
+        /// </summary>
+        public override void LoadDictionaries()
+        {
+            if (!Locale.LoadDictionaries(AppDirs.LangDir, ModuleUtils.ModuleCode, out string errMsg))
+                ScadaUiUtils.ShowError(errMsg);
+        }
+
+        /// <summary>
+        /// Indicates whether the module can create an archive of the specified kind.
+        /// </summary>
+        public override bool CanCreateArchive(ArchiveKind kind)
+        {
+            return kind == ArchiveKind.Current || kind == ArchiveKind.Historical || kind == ArchiveKind.Events;
+        }
+
+        /// <summary>
+        /// Creates a new archive user interface.
+        /// </summary>
+        public override ArchiveView CreateArchiveView(ArchiveConfig archiveConfig)
+        {
+            return archiveConfig.Kind switch
+            {
+                ArchiveKind.Current => new PostgreCAV(this, archiveConfig),
+                ArchiveKind.Historical => new PostgreHAV(this, archiveConfig),
+                ArchiveKind.Events => new PostgreEAV(this, archiveConfig),
+                _ => null
+            };
+        }
+    }
+}
---
+++ ScadaServer/OpenModules/ModArcPostgreSql.View/PostgreCAV.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Server.Archives;
+using Scada.Server.Config;
+using System.Windows.Forms;
+
+namespace Scada.Server.Modules.ModArcPostgreSql.View
+{
+    /// <summary>
+    /// Implements the current data archive user interface.
+    /// <para>Реализует пользовательский интерфейс архива текущих данных.</para>
+    /// </summary>
+    public class PostgreCAV : ArchiveView
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public PostgreCAV(ModuleView parentView, ArchiveConfig archiveConfig)
+            : base(parentView, archiveConfig)
+        {
+            CanShowProperties = true;
+        }
+
+        /// <summary>
+        /// Shows a modal dialog box for editing archive properties.
+        /// </summary>
+        public override bool ShowProperties()
+        {
+            //return new FrmCAO(AppDirs, ArchiveConfig).ShowDialog() == DialogResult.OK;
+            return false;
+        }
+    }
+}
