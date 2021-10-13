5b768e936916c60bd32ec53a9ee0b4846816b6aa
2mik <m_shir@mail.ru>
2021-10-13 10:07:50 +0300

    ExtCommConfig: main classes

---
+++ ScadaAdmin/OpenExtensions/ExtCommConfig/Code/ExtensionUtils.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Admin.Project;
+using Scada.Comm.Drivers;
+using System;
+
+namespace Scada.Admin.Extensions.ExtCommConfig.Code
+{
+    /// <summary>
+    /// The class provides helper methods for the extension.
+    /// <para>Класс, предоставляющий вспомогательные методы для расширения.</para>
+    /// </summary>
+    internal static class ExtensionUtils
+    {
+        /// <summary>
+        /// Gets a new instance of the module user interface.
+        /// </summary>
+        public static bool GetDriverView(IAdminContext adminContext, CommApp commApp, string driverCode,
+            out DriverView driverView, out string message)
+        {
+            if (adminContext == null)
+                throw new ArgumentNullException(nameof(adminContext));
+            if (commApp == null)
+                throw new ArgumentNullException(nameof(commApp));
+
+            if (DriverFactory.GetDriverView(adminContext.AppDirs.LibDir, driverCode, out driverView, out message))
+            {
+                driverView.BaseDataSet = adminContext.CurrentProject.ConfigBase;
+                driverView.AppDirs = adminContext.AppDirs.CreateDirsForView(commApp.ConfigDir);
+                driverView.AppConfig = commApp.AppConfig;
+                driverView.LoadDictionaries();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ScadaAdmin/OpenExtensions/ExtCommConfig/ExtCommConfigLogic.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Admin.Lang;
+using Scada.Admin.Project;
+using Scada.Lang;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Scada.Admin.Extensions.ExtCommConfig
+{
+    /// <summary>
+    /// Represents an extension logic.
+    /// <para>Представляет логику расширения.</para>
+    /// </summary>
+    public class ExtCommConfigLogic : ExtensionLogic
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public ExtCommConfigLogic(IAdminContext adminContext)
+            : base(adminContext)
+        {
+        }
+
+
+        /// <summary>
+        /// Gets the extension code.
+        /// </summary>
+        public override string Code
+        {
+            get
+            {
+                return "ExtCommConfig";
+            }
+        }
+
+
+        /// <summary>
+        /// Loads language dictionaries.
+        /// </summary>
+        public override void LoadDictionaries()
+        {
+            if (!Locale.LoadDictionaries(AdminContext.AppDirs.LangDir, Code, out string errMsg))
+                AdminContext.ErrLog.WriteError(AdminPhrases.ExtensionMessage, Code, errMsg);
+
+            //ExtensionPhrases.Init();
+        }
+
+        /// <summary>
+        /// Gets tree nodes to add to the explorer tree.
+        /// </summary>
+        public override TreeNode[] GetTreeNodes(object relatedObject)
+        {
+            if (relatedObject is not CommApp commApp)
+                return null;
+
+            return null;
+        }
+
+        /// <summary>
+        /// Gets the images used by the explorer tree.
+        /// </summary>
+        public override Dictionary<string, Image> GetTreeViewImages()
+        {
+            return null;
+        }
+    }
+}
