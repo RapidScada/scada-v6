946be3474e96f0ceb555be57ed2819db74242912
2mik <m_shir@mail.ru>
2021-10-14 10:43:53 +0300

    Initial commit of DrvDsScadaServer.View

---
+++ ScadaComm/OpenDrivers/DrvDsScadaServer.View/DrvDsScadaServerView.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Comm.Config;
+using Scada.Comm.DataSources;
+using Scada.Lang;
+
+namespace Scada.Comm.Drivers.DrvDsScadaServer.View
+{
+    /// <summary>
+    /// Implements the driver user interface.
+    /// <para>Реализует пользовательский интерфейс драйвера.</para>
+    /// </summary>
+    public class DrvDsScadaServerView : DriverView
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public DrvDsScadaServerView()
+            : base()
+        {
+            CanCreateDataSource = true;
+        }
+
+
+        /// <summary>
+        /// Gets the driver name.
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return Locale.IsRussian ? "Источник данных Сервер" : "Server Data Source";
+            }
+        }
+
+        /// <summary>
+        /// Gets the driver description.
+        /// </summary>
+        public override string Descr
+        {
+            get
+            {
+                return Locale.IsRussian ?
+                    "Обеспечивает связь между службами Коммуникатора и Сервера." :
+                    "Provides communication between the Communicator and Server services.";
+            }
+        }
+
+
+        /// <summary>
+        /// Creates a new data source user interface.
+        /// </summary>
+        public override DataSourceView CreateDataSourceView(DataSourceConfig dataSourceConfig)
+        {
+            return null;
+        }
+    }
+}
---
+++ ScadaComm/OpenDrivers/DrvDsScadaServer.View/ScadaServerDSV.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Comm.DataSources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scada.Comm.Drivers.DrvDsScadaServer.View
+{
+    /// <summary>
+    /// Implements the data source user interface.
+    /// <para>Реализует пользовательский интерфейс источника данных.</para>
+    /// </summary>
+    internal class ScadaServerDSV : DataSourceView
+    {
+    }
+}
