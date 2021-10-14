224cfc5f4404d1e554078e96989d6b3416ccd687
2mik <m_shir@mail.ru>
2021-10-14 16:26:32 +0300

    ModArcPostgeSql: remove usings

--- ScadaServer/OpenModules/ModArcPostgreSql.Common/ModuleConfig.cs
+++ ScadaServer/OpenModules/ModArcPostgreSql.Common/ModuleConfig.cs
@@ -2,9 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Scada.Config;
-using Scada.Lang;
-using Scada.Server.Lang;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Xml;
