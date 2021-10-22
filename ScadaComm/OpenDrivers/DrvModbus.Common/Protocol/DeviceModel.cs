02deeeed3a8f2b9906d3fc9f4ee5cbfc1d16be4f
2mik <m_shir@mail.ru>
2021-10-22 14:23:06 +0300

    DrvModbus: device model

--- ScadaComm/OpenDrivers/DrvModbus.Common/Protocol/DeviceModel.cs
+++ ScadaComm/OpenDrivers/DrvModbus.Common/Protocol/DeviceModel.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) Rapid Software LLC. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Scada.Comm.Drivers.DrvModbus.Protocol
 {
@@ -13,19 +11,30 @@ namespace Scada.Comm.Drivers.DrvModbus.Protocol
     /// </summary>
     public class DeviceModel
     {
-        /// <summary>
-        /// Найти команду по номеру
-        /// </summary>
-        public ModbusCmd FindCmd(int cmdNum)
-        {
-            /*foreach (ModbusCmd cmd in Cmds)
-            {
-                if (cmd.CmdNum == cmdNum)
-                    return cmd;
-            }*/
+        private Dictionary<int, ModbusCmd> cmdByNum; // the commands accessed by number
 
-            return null;
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public DeviceModel()
+        {
+            cmdByNum = null;
+
+            ElemGroups = new List<ElemGroup>();
+            Cmds = new List<ModbusCmd>();
         }
+
+
+        /// <summary>
+        /// Gets the element groups.
+        /// </summary>
+        public List<ElemGroup> ElemGroups { get; private set; }
+
+        /// <summary>
+        /// Gets the commands.
+        /// </summary>
+        public List<ModbusCmd> Cmds { get; private set; }
+
 
         /// <summary>
         /// Creates a new group of Modbus elements.
@@ -41,6 +50,28 @@ namespace Scada.Comm.Drivers.DrvModbus.Protocol
         public virtual ModbusCmd CreateModbusCmd(DataBlock dataBlock, bool multiple)
         {
             return new ModbusCmd(dataBlock, multiple);
+        }
+
+        /// <summary>
+        /// Initializes a command map.
+        /// </summary>
+        public void InitCmdMap()
+        {
+            cmdByNum = new Dictionary<int, ModbusCmd>();
+
+            foreach (ModbusCmd cmd in Cmds)
+            { 
+                if (cmdByNum.ContainsKey(cmd.CmdNum))
+                    cmdByNum.Add(cmd.CmdNum, cmd); 
+            };
+        }
+
+        /// <summary>
+        /// Gets a command by the specified number.
+        /// </summary>
+        public ModbusCmd GetCmd(int cmdNum)
+        {
+            return cmdByNum != null && cmdByNum.TryGetValue(cmdNum, out ModbusCmd cmd) ? cmd : null;
         }
     }
 }
