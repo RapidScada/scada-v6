19de5445be3c4f411f436357cc344365c6a0fcbb
2mik <m_shir@mail.ru>
2021-10-22 18:30:58 +0300

    DrvModbus: add templates, start logic dev

---
+++ ScadaComm/OpenDrivers/DrvModbus.Logic/DevModbusLogic.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Comm.Config;
+using Scada.Comm.Devices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scada.Comm.Drivers.DrvModbus.Logic
+{
+    /// <summary>
+    /// Implements the device logic.
+    /// <para>Реализует логику КП.</para>
+    /// </summary>
+    public class DevModbusLogic : DeviceLogic
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public DevModbusLogic(ICommContext commContext, ILineContext lineContext, DeviceConfig deviceConfig)
+            : base(commContext, lineContext, deviceConfig)
+        {
+        }
+    }
+}
