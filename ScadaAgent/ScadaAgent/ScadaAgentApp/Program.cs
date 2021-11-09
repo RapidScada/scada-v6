083e247645d3b3b4133bbbd72e4832c70d2268fe
2mik <m_shir@mail.ru>
2021-11-09 11:06:03 +0300

    ScadaAgent: solution structure

---
+++ ScadaAgent/ScadaAgent/ScadaAgentApp/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Copyright 2021 Rapid Software LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Product  : Rapid SCADA
+ * Module   : Agent Application
+ * Summary  : The Agent cross-platform console application
+ * 
+ * Author   : Mikhail Shiryaev
+ * Created  : 2021
+ * Modified : 2021
+ */
+
+using System;
+
+namespace Scada.Agent.App
+{
+    /// <summary>
+    /// The Agent cross-platform console application.
+    /// <para>Кросс-платформенное консольное приложение Агента.</para>
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Agent...");
+            Console.WriteLine("Agent is stopped");
+        }
+    }
+}
