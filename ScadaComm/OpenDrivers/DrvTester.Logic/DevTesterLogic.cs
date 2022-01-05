28d88710d634e4b96bec3a691d0e89013876a6c7
2mik <m_shir@mail.ru>
2022-01-05 14:52:29 +0300

    Initial commit of DrvTester

---
+++ ScadaComm/OpenDrivers/DrvTester.Logic/DevTesterLogic.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Comm.Config;
+using Scada.Comm.Devices;
+using Scada.Comm.Lang;
+using Scada.Data.Models;
+using System;
+
+namespace Scada.Comm.Drivers.DrvTester.Logic
+{
+    /// <summary>
+    /// Implements the device logic.
+    /// <para>Реализует логику КП.</para>
+    /// </summary>
+    internal class DevTesterLogic : DeviceLogic
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public DevTesterLogic(ICommContext commContext, ILineContext lineContext, DeviceConfig deviceConfig)
+            : base(commContext, lineContext, deviceConfig)
+        {
+            CanSendCommands = true;
+        }
+
+        /// <summary>
+        /// Sends the telecontrol command.
+        /// </summary>
+        public override void SendCommand(TeleCommand cmd)
+        {
+            base.SendCommand(cmd);
+
+            if (cmd.CmdCode == "SendBin" || cmd.CmdNum == 1)
+            {
+                byte[] buffer = cmd.CmdData ?? Array.Empty<byte>();
+                Connection.Write(buffer, 0, buffer.Length);
+            }
+            else if (cmd.CmdCode == "SendStr" || cmd.CmdNum == 2)
+            {
+                Connection.WriteLine(cmd.GetCmdDataString());
+            }
+            else
+            {
+                LastRequestOK = false;
+                Log.WriteLine(CommPhrases.InvalidCommand);
+            }
+
+            FinishCommand();
+        }
+    }
+}
---
+++ ScadaComm/OpenDrivers/DrvTester.View/DrvTesterView.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Lang;
+
+namespace Scada.Comm.Drivers.DrvTester.View
+{
+    /// <summary>
+    /// Implements the driver user interface.
+    /// <para>Реализует пользовательский интерфейс драйвера.</para>
+    /// </summary>
+    public class DrvTesterView : DriverView
+    {
+        /// <summary>
+        /// Gets the driver name.
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return Locale.IsRussian ? "Тестер каналов связи" : "Communication Channel Tester";
+            }
+        }
+
+        /// <summary>
+        /// Gets the driver description.
+        /// </summary>
+        public override string Descr
+        {
+            get
+            {
+                return Locale.IsRussian ?
+                    "Предназначен для проверки каналов связи.\n\n" +
+                    "Команды ТУ:\n" +
+                    "1, SendBin - отправить бинарные данные;\n" +
+                    "2, SendStr - отправить строку." :
+
+                    "Designed for testing communication channels.\n\n" +
+                    "Commands:\n" +
+                    "1, SendBin - send binary data;\n" +
+                    "2, SendStr - send string.";
+            }
+        }
+    }
+}
