9b1693ef92003b24a3c79d9b4eea926433dbd175
2mik <m_shir@mail.ru>
2022-01-06 17:54:38 +0300

    ScadaComm: update comments

--- ScadaComm/ScadaComm/ScadaCommCommon/Channels/Connection.cs
+++ ScadaComm/ScadaComm/ScadaCommCommon/Channels/Connection.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2021 Rapid Software LLC
+ * Copyright 2022 Rapid Software LLC
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@
  * 
  * Author   : Mikhail Shiryaev
  * Created  : 2015
- * Modified : 2020
+ * Modified : 2022
  */
 
 using Scada.Comm.Lang;
