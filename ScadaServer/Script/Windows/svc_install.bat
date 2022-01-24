320fe94d514b814d81cedb47a5d5a6258e97779c
2mik <m_shir@mail.ru>
2022-01-24 17:48:24 +0300

    Update svc_install.bat

--- ScadaServer/Script/Windows/svc_install.bat
+++ ScadaServer/Script/Windows/svc_install.bat
@@ -1,3 +1,3 @@
 SET filepath="%cd%\ScadaServerWkr.exe"
-sc create ScadaServer6 binPath= %filepath%
+sc create ScadaServer6 binPath= %filepath% start= auto
 sc description ScadaServer6 "Rapid SCADA Server"
