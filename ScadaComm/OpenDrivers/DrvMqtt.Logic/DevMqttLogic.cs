417e34d54a39b959e9e8da3c626544d7e3c02bf0
2mik <m_shir@mail.ru>
2022-03-11 19:30:16 +0300

    DrvMqtt: test mqtt

---
+++ ScadaComm/OpenDrivers/DrvMqtt.Logic/DevMqttLogic.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Subscribing;
+using Scada.Comm.Config;
+using Scada.Comm.Devices;
+
+namespace Scada.Comm.Drivers.DrvMqtt.Logic
+{
+    /// <summary>
+    /// Implements the device logic.
+    /// <para>Реализует логику устройства.</para>
+    /// </summary>
+    internal class DevMqttLogic : DeviceLogic
+    {
+        private IMqttClient mqttClient;
+
+
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public DevMqttLogic(ICommContext commContext, ILineContext lineContext, DeviceConfig deviceConfig)
+            : base(commContext, lineContext, deviceConfig)
+        {
+            ConnectionRequired = false;
+        }
+
+
+        /// <summary>
+        /// Performs actions when starting a communication line.
+        /// </summary>
+        public override void OnCommLineStart()
+        {
+            MqttFactory mqttFactory = new();
+            mqttClient = mqttFactory.CreateMqttClient();
+
+            mqttClient.UseApplicationMessageReceivedHandler(e =>
+            {
+                Log.WriteLine("!!! Application message = " + e.ApplicationMessage.Topic + ", " + 
+                    e.ApplicationMessage.ConvertPayloadToString());
+            });
+
+            IMqttClientOptions mqttClientOptions = new MqttClientOptionsBuilder()
+                .WithTcpServer("broker.mqtt-dashboard.com")
+                .WithClientId("rapidscada")
+                .Build();
+
+            Task<MqttClientConnectResult> connectTask = mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+            connectTask.Wait();
+            Log.WriteLine("!!! Connect result = " + connectTask.Result);
+
+            MqttClientSubscribeOptions mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+                .WithTopicFilter(f => { f.WithTopic("/myparam1"); })
+                .Build();
+
+            mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None).Wait();
+            Log.WriteLine("!!! MQTT client subscribed to topic.");
+        }
+
+        /// <summary>
+        /// Performs actions when terminating a communication line.
+        /// </summary>
+        public override void OnCommLineTerminate()
+        {
+            if (mqttClient != null)
+            {
+                mqttClient.DisconnectAsync().Wait();
+                mqttClient.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Performs a communication session.
+        /// </summary>
+        public override void Session()
+        {
+            //base.Session();
+            //FinishSession();
+        }
+    }
+}
--- ScadaComm/OpenDrivers/DrvMqtt.Logic/DrvMqttLogic.cs
+++ ScadaComm/OpenDrivers/DrvMqtt.Logic/DrvMqttLogic.cs
@@ -1,10 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Comm.Config;
+using Scada.Comm.Devices;
 
 namespace Scada.Comm.Drivers.DrvMqtt.Logic
 {
-    public class DrvMqttLogic
+    /// <summary>
+    /// Implements the driver logic.
+    /// <para>Реализует логику драйвера.</para>
+    /// </summary>
+    public class DrvMqttLogic : DriverLogic
     {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public DrvMqttLogic(ICommContext commContext)
+            : base(commContext)
+        {
+        }
+
+        /// <summary>
+        /// Gets the driver code.
+        /// </summary>
+        public override string Code
+        {
+            get
+            {
+                return "DrvMqtt";
+            }
+        }
+
+        /// <summary>
+        /// Creates a new device.
+        /// </summary>
+        public override DeviceLogic CreateDevice(ILineContext lineContext, DeviceConfig deviceConfig)
+        {
+            return new DevMqttLogic(CommContext, lineContext, deviceConfig);
+        }
     }
 }
