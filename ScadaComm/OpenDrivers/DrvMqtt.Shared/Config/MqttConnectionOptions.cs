ce6dc71a0e788ec5da3497d1b14f3ba8c0e136b1
2mik <m_shir@mail.ru>
2022-03-15 15:56:46 +0300

    DrvMqtt: config

---
+++ ScadaComm/OpenDrivers/DrvMqtt.Shared/Config/MqttConnectionOptions.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scada.Comm.Drivers.DrvMqtt.Config
+{
+    /// <summary>
+    /// Represents options for connecting to an MQTT broker.
+    /// <para>Представляет параметры подключения к MQTT-брокеру.</para>
+    /// </summary>
+    internal class MqttConnectionOptions
+    {
+    }
+}
