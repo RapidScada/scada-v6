9b67db18edc00296932ffcbf6cef3e45d60e1b9c
2mik <m_shir@mail.ru>
2022-03-23 17:02:51 +0300

    DrvDsMqtt: driver structure

--- ScadaComm/OpenDrivers/DrvDsMqtt.Logic/DrvDsMqttLogic.cs
+++ ScadaComm/OpenDrivers/DrvDsMqtt.Logic/DrvDsMqttLogic.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Comm.Config;
+using Scada.Comm.DataSources;
 
 namespace Scada.Comm.Drivers.DrvDsMqtt.Logic
 {
-    public class DrvDsMqttLogic
+    /// <summary>
+    /// Implements the driver logic.
+    /// <para>Реализует логику драйвера.</para>
+    /// </summary>
+    public class DrvDsMqttLogic : DriverLogic
     {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public DrvDsMqttLogic(ICommContext commContext)
+            : base(commContext)
+        {
+        }
+
+        /// <summary>
+        /// Gets the driver code.
+        /// </summary>
+        public override string Code
+        {
+            get
+            {
+                return "DrvDsMqttLogic";
+            }
+        }
+
+        /// <summary>
+        /// Creates a new data source.
+        /// </summary>
+        public override DataSourceLogic CreateDataSource(ICommContext commContext, DataSourceConfig dataSourceConfig)
+        {
+            return new MqttDSL(commContext, dataSourceConfig);
+        }
     }
 }
---
+++ ScadaComm/OpenDrivers/DrvDsMqtt.Logic/MqttDSL.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Comm.Config;
+using Scada.Comm.DataSources;
+
+namespace Scada.Comm.Drivers.DrvDsMqtt.Logic
+{
+    /// <summary>
+    /// Implements the data source logic.
+    /// <para>Реализует логику источника данных.</para>
+    /// </summary>
+    internal class MqttDSL : DataSourceLogic
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public MqttDSL(ICommContext commContext, DataSourceConfig dataSourceConfig)
+            : base(commContext, dataSourceConfig)
+        {
+            //options = new OpcUaServerDSO(dataSourceConfig.CustomOptions);
+            //dsLog = CreateLog(DriverUtils.DriverCode);
+        }
+    }
+}
