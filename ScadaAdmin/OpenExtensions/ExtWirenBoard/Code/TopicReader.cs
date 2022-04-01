d60bf4c372206043aca70e0e8d5fbde01c4d9b2e
2mik <m_shir@mail.ru>
2022-04-01 14:59:08 +0300

    ExtWirenBoard: read topics

--- ScadaAdmin/OpenExtensions/ExtWirenBoard/Code/TopicReader.cs
+++ ScadaAdmin/OpenExtensions/ExtWirenBoard/Code/TopicReader.cs
@@ -4,6 +4,7 @@
 using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Subscribing;
 using Scada.Comm.Drivers.DrvMqtt;
 using Scada.Forms;
 using Scada.Lang;
@@ -16,9 +17,22 @@ namespace Scada.Admin.Extensions.ExtWirenBoard.Code
     /// </summary>
     internal class TopicReader
     {
+        /// <summary>
+        /// The topic for receiving information about Wiren Board devices and controls.
+        /// </summary>
+        private const string MainTopic = "/devices/#";
+        /// <summary>
+        /// Specifies how long to wait for new data after reading the latest data.
+        /// </summary>
+        private readonly TimeSpan ReadTimeout = TimeSpan.FromSeconds(3);
+
         private readonly MqttConnectionOptions connOptions; // the connection options
         private readonly RichTextBoxHelper logHelper;       // provides access to log
+        private readonly object mqttLock;                   // synchronizes reading
+
         private CancellationTokenSource tokenSource;        // provides a cancellation mechanism
+        private DateTime messageDT;                         // the timestamp of the last received message
+        private bool stopReading;                           // indicates that a read operation should stop
 
 
         /// <summary>
@@ -28,7 +42,12 @@ namespace Scada.Admin.Extensions.ExtWirenBoard.Code
         {
             connOptions = mqttConnectionOptions ?? throw new ArgumentNullException(nameof(mqttConnectionOptions));
             this.logHelper = logHelper ?? throw new ArgumentNullException(nameof(logHelper));
+            mqttLock = new object();
+
             tokenSource = null;
+            messageDT = DateTime.MinValue;
+            stopReading = false;
+
             ReadResult = false;
         }
 
@@ -48,7 +67,10 @@ namespace Scada.Admin.Extensions.ExtWirenBoard.Code
 
             try
             {
-                mqttClient = new MqttFactory().CreateMqttClient();
+                MqttFactory mqttFactory = new();
+                mqttClient = mqttFactory.CreateMqttClient();
+                mqttClient.UseApplicationMessageReceivedHandler(MqttClient_ApplicationMessageReceived);
+
                 logHelper.WriteMessage(string.Format(Locale.IsRussian ?
                     "Соединение с {0}:{1}" :
                     "Connect to {0}:{1}",
@@ -60,9 +82,36 @@ namespace Scada.Admin.Extensions.ExtWirenBoard.Code
 
                 if (resultCode == MqttClientConnectResultCode.Success)
                 {
-                    logHelper.WriteMessage(string.Format(Locale.IsRussian ?
+                    logHelper.WriteMessage(Locale.IsRussian ?
                         "Соединение установлено успешно" :
-                        "Connected successfully"));
+                        "Connected successfully");
+
+                    logHelper.WriteMessage(string.Format(Locale.IsRussian ?
+                        "Подписка на {0}" :
+                        "Subscribe to {0}", MainTopic));
+                    MqttClientSubscribeOptions subscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+                        .WithTopicFilter(f => f.WithTopic(MainTopic)).Build();
+                    mqttClient.SubscribeAsync(subscribeOptions, tokenSource.Token).Wait();
+
+                    while (!stopReading && !tokenSource.IsCancellationRequested)
+                    {
+                        Thread.Sleep(ScadaUtils.ThreadDelay);
+
+                        lock (mqttLock)
+                        {
+                            if (DateTime.UtcNow - messageDT > ReadTimeout)
+                            {
+                                stopReading = true;
+                                logHelper.WriteMessage(Locale.IsRussian ?
+                                    "Таймаут чтения истёк" :
+                                    "Read timeout expired");
+                            }
+                        }
+                    }
+
+                    logHelper.WriteSuccess(Locale.IsRussian ?
+                        "Чтение топиков завершено успешно" :
+                        "Reading topics completed successfully");
                     ReadResult = true;
                 }
                 else
@@ -86,7 +135,7 @@ namespace Scada.Admin.Extensions.ExtWirenBoard.Code
         /// <summary>
         /// Disconnects and disposes the MQTT client.
         /// </summary>
-        public void Disconnect(IMqttClient mqttClient)
+        private void Disconnect(IMqttClient mqttClient)
         {
             if (mqttClient == null || !mqttClient.IsConnected)
                 return;
@@ -118,6 +167,36 @@ namespace Scada.Admin.Extensions.ExtWirenBoard.Code
         {
             Completed?.Invoke(this, EventArgs.Empty);
         }
+
+        /// <summary>
+        /// Handles a message received event.
+        /// </summary>
+        private void MqttClient_ApplicationMessageReceived(MqttApplicationMessageReceivedEventArgs e)
+        {
+            try
+            {
+                lock (mqttLock)
+                {
+                    messageDT = DateTime.UtcNow;
+
+                    logHelper.WriteMessage(string.Format("{0} {1}",
+                        e.ApplicationMessage.Topic, e.ApplicationMessage.ConvertPayloadToString()));
+
+                    if (!e.ApplicationMessage.Retain)
+                    {
+                        stopReading = true;
+                        logHelper.WriteMessage(Locale.IsRussian ?
+                            "Получен маркер завершения чтения" :
+                            "Read completion marker received");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logHelper.WriteError(ex.Message);
+            }
+        }
+
 
         /// <summary>
         /// Starts reading topics.
