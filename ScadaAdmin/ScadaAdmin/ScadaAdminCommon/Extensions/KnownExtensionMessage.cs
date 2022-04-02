bf4cdeffb6ec0dd926378ef793411fbcbf9d500f
2mik <m_shir@mail.ru>
2022-04-02 10:48:00 +0300

    Create KnownExtensionMessage.cs

---
+++ ScadaAdmin/ScadaAdmin/ScadaAdminCommon/Extensions/KnownExtensionMessage.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Copyright 2022 Rapid Software LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Product  : Rapid SCADA
+ * Module   : ScadaAdminCommon
+ * Summary  : Specifies the messages sent and received by the Administrator application
+ * 
+ * Author   : Mikhail Shiryaev
+ * Created  : 2022
+ * Modified : 2022
+ */
+
+namespace Scada.Admin.Extensions
+{
+    /// <summary>
+    /// Specifies the known messages sent and received by extensions.
+    /// <para>Задает известные сообщения, отправляемые и принимаемые расширениями.</para>
+    /// </summary>
+    public static class KnownExtensionMessage
+    {
+        /// <summary>
+        /// Calls generation of a channel map by the Project Tools extension.
+        /// </summary>
+        public const string GenerateChannelMap = "ExtProjectTools.GenerateChannelMap";
+
+        /// <summary>
+        /// Calls generation of a device map by the Project Tools extension.
+        /// </summary>
+        public const string GenerateDeviceMap = "ExtProjectTools.GenerateDeviceMap";
+    }
+}
