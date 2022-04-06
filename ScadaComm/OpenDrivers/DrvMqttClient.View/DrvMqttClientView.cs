5b830a36c8f5be303e124e2d6d2791cfe149c8e0
2mik <m_shir@mail.ru>
2022-04-06 10:58:19 +0300

    Create DrvMqttClient.View project

---
+++ ScadaComm/OpenDrivers/DrvMqttClient.View/DrvMqttClientView.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Lang;
+
+namespace Scada.Comm.Drivers.DrvMqttClient.View
+{
+    /// <summary>
+    /// Implements the driver user interface.
+    /// <para>Реализует пользовательский интерфейс драйвера.</para>
+    /// </summary>
+    public class DrvMqttClientView : DriverView
+    {
+        /// <summary>
+        /// Gets the driver name.
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return Locale.IsRussian ? "MQTT-клиент" : "MQTT Client";
+            }
+        }
+
+        /// <summary>
+        /// Gets the driver description.
+        /// </summary>
+        public override string Descr
+        {
+            get
+            {
+                return Locale.IsRussian ?
+                    "Получает данные устройств от MQTT-брокера и публикует команды." :
+                    "Receives device data from MQTT broker and publishes commands.";
+            }
+        }
+    }
+}
