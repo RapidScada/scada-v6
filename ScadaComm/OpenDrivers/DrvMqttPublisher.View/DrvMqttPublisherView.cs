beaac9fe37164067e5667d0fcb5672f070cb1ef7
2mik <m_shir@mail.ru>
2022-04-06 11:09:58 +0300

    Create DrvMqttPublisher project

---
+++ ScadaComm/OpenDrivers/DrvMqttPublisher.View/DrvMqttPublisherView.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Lang;
+
+namespace Scada.Comm.Drivers.DrvMqttPublisher.View
+{
+    /// <summary>
+    /// Implements the driver user interface.
+    /// <para>Реализует пользовательский интерфейс драйвера.</para>
+    /// </summary>
+    public class DrvMqttPublisherView : DriverView
+    {
+        /// <summary>
+        /// Gets the driver name.
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return Locale.IsRussian ? "MQTT-издатель" : "MQTT Publisher";
+            }
+        }
+
+        /// <summary>
+        /// Gets the driver description.
+        /// </summary>
+        public override string Descr
+        {
+            get
+            {
+                return Locale.IsRussian ?
+                    "Транслирует данные каналов на MQTT-брокер и принимает команды." :
+                    "Transmits channel data to MQTT broker and receives commands.";
+            }
+        }
+    }
+}
