828f9f50cecbfa8879dfc1675b5de805a5b93142
2mik <m_shir@mail.ru>
2022-04-15 16:55:45 +0300

    DrvHttpNotif: dev

---
+++ ScadaComm/OpenDrivers/DrvHttpNotif.Logic/DevHttpNotifLogic.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Comm.Config;
+using Scada.Comm.Devices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scada.Comm.Drivers.DrvHttpNotif.Logic
+{
+    /// <summary>
+    /// Implements the device logic.
+    /// <para>Реализует логику устройства.</para>
+    /// </summary>
+    internal class DevHttpNotifLogic : DeviceLogic
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public DevHttpNotifLogic(ICommContext commContext, ILineContext lineContext, DeviceConfig deviceConfig)
+            : base(commContext, lineContext, deviceConfig)
+        {
+            CanSendCommands = true;
+            ConnectionRequired = false;
+        }
+    }
+}
--- ScadaComm/OpenDrivers/DrvHttpNotif.Logic/DrvHttpNotifLogic.cs
+++ ScadaComm/OpenDrivers/DrvHttpNotif.Logic/DrvHttpNotifLogic.cs
@@ -1,10 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Comm.Config;
+using Scada.Comm.Devices;
 
 namespace Scada.Comm.Drivers.DrvHttpNotif.Logic
 {
-    public class DrvHttpNotifLogic
+    /// <summary>
+    /// Implements the driver logic.
+    /// <para>Реализует логику драйвера.</para>
+    /// </summary>
+    public class DrvHttpNotifLogic : DriverLogic
     {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public DrvHttpNotifLogic(ICommContext commContext)
+            : base(commContext)
+        {
+        }
+
+        /// <summary>
+        /// Gets the driver code.
+        /// </summary>
+        public override string Code
+        {
+            get
+            {
+                return DriverUtils.DriverCode;
+            }
+        }
+
+        /// <summary>
+        /// Creates a new device.
+        /// </summary>
+        public override DeviceLogic CreateDevice(ILineContext lineContext, DeviceConfig deviceConfig)
+        {
+            return new DevHttpNotifLogic(CommContext, lineContext, deviceConfig);
+        }
     }
 }
