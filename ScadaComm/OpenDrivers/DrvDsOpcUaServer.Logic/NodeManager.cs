79086be364b57d984f3654c13712205a93b7bdd7
2mik <m_shir@mail.ru>
2022-04-15 12:09:39 +0300

    DrvDsOpcUaServer: fix node ids

--- ScadaComm/OpenDrivers/DrvDsOpcUaServer.Logic/NodeManager.cs
+++ ScadaComm/OpenDrivers/DrvDsOpcUaServer.Logic/NodeManager.cs
@@ -1,27 +1,5 @@
-﻿/*
- * Copyright 2021 Rapid Software LLC
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- * 
- * Product  : Rapid SCADA
- * Module   : DrvDsOpcUaServer
- * Summary  : Represents nodes of OPC UA server
- * 
- * Author   : Mikhail Shiryaev
- * Created  : 2021
- * Modified : 2021
- */
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Opc.Ua;
 using Opc.Ua.Server;
@@ -205,8 +183,8 @@ namespace Scada.Comm.Drivers.DrvDsOpcUaServer.Logic
 
                     foreach (DeviceLogic deviceLogic in devices)
                     {
-                        FolderState deviceFolder = CreateFolder(lineFolder,
-                            CommUtils.GetDeviceLogFileName(deviceLogic.DeviceNum, ""), deviceLogic.Title);
+                        string deviceFolderName = CommUtils.GetDeviceLogFileName(deviceLogic.DeviceNum, "");
+                        FolderState deviceFolder = CreateFolder(lineFolder, deviceFolderName, deviceLogic.Title);
                         DeviceVars deviceVars = new DeviceVars();
                         varByDevice[deviceLogic.DeviceNum] = deviceVars;
 
@@ -223,8 +201,8 @@ namespace Scada.Comm.Drivers.DrvDsOpcUaServer.Logic
 
                             foreach (TagGroup tagGroup in deviceLogic.DeviceTags.TagGroups)
                             {
-                                FolderState groupFolder = CreateFolder(deviceFolder, 
-                                    "group" + groupNum.ToString("D3"), tagGroup.Name);
+                                FolderState groupFolder = CreateFolder(deviceFolder,
+                                    deviceFolderName + ".group" + groupNum.ToString("D3"), tagGroup.Name);
                                 CreateDeviceTagNodes(groupFolder, deviceLogic, tagGroup, deviceVars);
                                 groupNum++;
                             }
