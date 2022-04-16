6d2816633b4a16b45595bef2ade45240f0aef645
2mik <m_shir@mail.ru>
2022-04-16 13:21:20 +0300

    AddressBook: phrases

---
+++ ScadaComm/OpenDrivers2/AddressBooks.Forms/Lang/AddressBookPhrases.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Lang;
+
+namespace Scada.AB.Forms.Lang
+{
+    /// <summary>
+    /// The phrases used by the address book.
+    /// <para>Фразы, используемые адресной книгой.</para>
+    /// </summary>
+    public class AddressBookPhrases
+    {
+        // Scada.AB.Forms.FrmAddressBook
+        public static string AddressBookNode { get; private set; }
+        public static string NewContactGroup { get; private set; }
+        public static string NewContact { get; private set; }
+        public static string NewPhoneNumber { get; private set; }
+        public static string NewEmail { get; private set; }
+        public static string ContactGroupExists { get; private set; }
+        public static string ContactExists { get; private set; }
+        public static string PhoneNumberExists { get; private set; }
+        public static string EmailExists { get; private set; }
+        public static string IncorrectEmail { get; private set; }
+        public static string EmptyValueNotAllowed { get; private set; }
+        public static string SavePhonebookConfirm { get; private set; }
+
+        public static void Init()
+        {
+            LocaleDict dict = Locale.GetDictionary("Scada.AB.Forms.FrmAddressBook");
+            AddressBookNode = dict[nameof(AddressBookNode)];
+            NewContactGroup = dict[nameof(NewContactGroup)];
+            NewContact = dict[nameof(NewContact)];
+            NewPhoneNumber = dict[nameof(NewPhoneNumber)];
+            NewEmail = dict[nameof(NewEmail)];
+            ContactGroupExists = dict[nameof(ContactGroupExists)];
+            ContactExists = dict[nameof(ContactExists)];
+            PhoneNumberExists = dict[nameof(PhoneNumberExists)];
+            EmailExists = dict[nameof(EmailExists)];
+            IncorrectEmail = dict[nameof(IncorrectEmail)];
+            EmptyValueNotAllowed = dict[nameof(EmptyValueNotAllowed)];
+            SavePhonebookConfirm = dict[nameof(SavePhonebookConfirm)];
+        }
+    }
+}
