58fd7b6cc149f9648916c9a19c4a0db840535b95
2mik <m_shir@mail.ru>
2022-05-21 14:40:47 +0300

    ScadaWeb: design of select channels form

--- ScadaWeb/ScadaWeb/ScadaWeb/Pages/CnlSelect.cshtml.cs
+++ ScadaWeb/ScadaWeb/ScadaWeb/Pages/CnlSelect.cshtml.cs
@@ -1,7 +1,16 @@
 ﻿// Copyright (c) Rapid Software LLC. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Scada.Data.Entities;
+using Scada.Web.Api;
+using Scada.Web.Authorization;
+using Scada.Web.Components;
+using Scada.Web.Services;
+using System.Collections.Generic;
 
 namespace Scada.Web.Pages
 {
@@ -9,10 +18,36 @@ namespace Scada.Web.Pages
     /// Represents a page for selecting channels of the configuration database.
     /// <para>Представляет страницу для выбора каналов базы конфигурации.</para>
     /// </summary>
+    [Authorize(Policy = PolicyName.Restricted)]
     public class CnlSelectModel : PageModel
     {
-        public void OnGet()
+        private readonly IWebContext webContext;
+
+
+        public CnlSelectModel(IWebContext webContext)
         {
+            this.webContext = webContext;
+        }
+
+
+        public ModalPostbackArgs PostbackArgs { get; private set; } = null;
+        public List<SelectListItem> ObjList { get; private set; } = new();
+        public List<Cnl> SelectedCnls { get; private set; } = new();
+
+        [BindProperty]
+        public int ObjNum { get; set; }
+
+        [BindProperty]
+        public List<int> SelectedCnlNums { get; set; }
+
+
+        public void OnGet(IdList cnlNums)
+        {
+            foreach (int cnlNum in cnlNums)
+            {
+                if (webContext.ConfigBase.CnlTable.GetItem(cnlNum) is Cnl cnl)
+                    SelectedCnls.Add(cnl);
+            }
         }
     }
 }
--- ScadaWeb/ScadaWeb/ScadaWeb/wwwroot/js/dialogs.js
+++ ScadaWeb/ScadaWeb/ScadaWeb/wwwroot/js/dialogs.js
@@ -23,7 +23,7 @@ class Dialogs {
     selectChannels(selectedCnlNums, callback) {
         this._getModalManager().showModal(
             this.rootPath + "CnlSelect?cnlNums=" + selectedCnlNums,
-            new ModalOptions(ModalButton.OK_CANCEL),
+            new ModalOptions(ModalButton.OK_CANCEL, ModalSize.LARGE),
             callback);
     }
 }
