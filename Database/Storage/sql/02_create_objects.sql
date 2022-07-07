dbb68722eb96104416e5f881d38ec14afe097871
2mik <m_shir@mail.ru>
2022-07-07 16:34:36 +0300

    Update 02_create_objects.sql

--- Database/Storage/sql/02_create_objects.sql
+++ Database/Storage/sql/02_create_objects.sql
@@ -1,6 +1,6 @@
-\i 01_schema.sql
-\i 02_app.sql
-\i 03_app_config.sql
-\i 04_app_storage.sql
-\i 05_view_file.sql
+\i 03_schema.sql
+\i 04_app.sql
+\i 05_app_config.sql
+\i 06_app_storage.sql
+\i 07_view_file.sql
 \i inserts.sql
