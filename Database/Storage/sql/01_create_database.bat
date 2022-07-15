f2e99be138a299e62c032bd87faf617984de60fd
2mik <m_shir@mail.ru>
2022-07-15 14:37:27 +0300

    Update Postgres version

--- Database/Storage/sql/01_create_database.bat
+++ Database/Storage/sql/01_create_database.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\PostgreSQL\13\bin\psql.exe" -a -U postgres -f 01_create_database.sql 2> 01_create_database.err
+"C:\Program Files\PostgreSQL\14\bin\psql.exe" -a -U postgres -f 01_create_database.sql 2> 01_create_database.err
--- Database/Storage/sql/02_create_objects.bat
+++ Database/Storage/sql/02_create_objects.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\PostgreSQL\13\bin\psql.exe" -a -U postgres -d rapid_scada -f 02_create_objects.sql 2> 02_create_objects.err
+"C:\Program Files\PostgreSQL\14\bin\psql.exe" -a -U postgres -d rapid_scada -f 02_create_objects.sql 2> 02_create_objects.err
