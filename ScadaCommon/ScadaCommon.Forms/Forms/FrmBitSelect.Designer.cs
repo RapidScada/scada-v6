0088c94de3ab96b8cf3db00f79c718aad3a6846f
2mik <m_shir@mail.ru>
2022-09-07 18:41:38 +0300

    ScadaCommon: add bit select form

---
+++ ScadaCommon/ScadaCommon.Forms/Forms/FrmBitSelect.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Scada.Forms.Forms
+{
+    partial class FrmBitSelect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbBits = new System.Windows.Forms.CheckedListBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbBits
+            // 
+            this.lbBits.CheckOnClick = true;
+            this.lbBits.FormattingEnabled = true;
+            this.lbBits.IntegralHeight = false;
+            this.lbBits.Location = new System.Drawing.Point(12, 12);
+            this.lbBits.Name = "lbBits";
+            this.lbBits.Size = new System.Drawing.Size(360, 350);
+            this.lbBits.TabIndex = 0;
+            this.lbBits.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.lbBits_ItemCheck);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(297, 378);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(216, 378);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // FrmBitSelect
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(384, 413);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.lbBits);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmBitSelect";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Select Bit";
+            this.Load += new System.EventHandler(this.FrmBitSelect_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox lbBits;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
---
+++ ScadaCommon/ScadaCommon.Forms/Forms/FrmBitSelect.cs
@@ -0,0 +1,113 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace Scada.Forms.Forms
+{
+    /// <summary>
+    /// Represents a form for selecting a single bit.
+    /// <para>Представляет форму для выбора одного бита.</para>
+    /// </summary>
+    public partial class FrmBitSelect : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public FrmBitSelect()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Gets or sets the selected bit.
+        /// </summary>
+        public int SelectedBit { get; set; }
+
+        /// <summary>
+        /// Gets or sets the available bits.
+        /// </summary>
+        /// <remarks>This is a collection of BitItem or objects of any type.</remarks>
+        public object Bits { get; set; }
+
+
+        /// <summary>
+        /// Shows the list of bits.
+        /// </summary>
+        private void ShowBits()
+        {
+            if (Bits is IEnumerable bits)
+            {
+                try
+                {
+                    lbBits.BeginUpdate();
+                    lbBits.ItemCheck -= lbBits_ItemCheck;
+                    int index = 0;
+                    bool found = false;
+
+                    foreach (object bitObj in bits)
+                    {
+                        if (bitObj != null)
+                        {
+                            BitItem bitItem = bitObj as BitItem ?? new BitItem(index, bitObj.ToString());
+                            lbBits.Items.Add(bitItem);
+
+                            if (bitItem.Bit == SelectedBit)
+                            {
+                                lbBits.SetItemChecked(index, true);
+                                found = true;
+                            }
+                        }
+
+                        index++;
+                    }
+
+                    btnOK.Enabled = found;
+                }
+                finally
+                {
+                    lbBits.EndUpdate();
+                    lbBits.ItemCheck += lbBits_ItemCheck;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Deselects the list items.
+        /// </summary>
+        private void DeselectItems(int excludingIndex)
+        {
+            lbBits.ItemCheck -= lbBits_ItemCheck;
+
+            for (int i = 0, cnt = lbBits.Items.Count; i < cnt; i++)
+            {
+                if (i != excludingIndex)
+                    lbBits.SetItemChecked(i, false);
+            }
+
+            lbBits.ItemCheck += lbBits_ItemCheck;
+        }
+
+
+        private void FrmBitSelect_Load(object sender, EventArgs e)
+        {
+            FormTranslator.Translate(this, GetType().FullName);
+            ShowBits();
+        }
+
+        private void lbBits_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (e.NewValue == CheckState.Checked)
+            {
+                BitItem bitItem = (BitItem)lbBits.Items[e.Index];
+                SelectedBit = bitItem.Bit;
+                DeselectItems(e.Index);
+            }
+
+            btnOK.Enabled = lbBits.CheckedItems.Count > 0;
+        }
+    }
+}
