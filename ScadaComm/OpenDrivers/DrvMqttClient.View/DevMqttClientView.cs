ab351b323ee87d0af3b94f6b6fabde9da267e983
2mik <m_shir@mail.ru>
2022-09-09 12:44:07 +0300

    DrvMqttClient: channel prototypes

--- ScadaComm/OpenDrivers/DrvMqttClient.View/DevMqttClientView.cs
+++ ScadaComm/OpenDrivers/DrvMqttClient.View/DevMqttClientView.cs
@@ -3,6 +3,9 @@
 
 using Scada.Comm.Config;
 using Scada.Comm.Devices;
+using Scada.Comm.Drivers.DrvMqttClient.Config;
+using Scada.Data.Const;
+using Scada.Data.Models;
 using Scada.Forms.Forms;
 
 namespace Scada.Comm.Drivers.DrvMqttClient.View
@@ -45,7 +48,60 @@ namespace Scada.Comm.Drivers.DrvMqttClient.View
         /// </summary>
         public override ICollection<CnlPrototype> GetCnlPrototypes()
         {
-            return null;
+            MqttClientDeviceConfig config = new();
+
+            if (!config.Load(Path.Combine(AppDirs.ConfigDir, MqttClientDeviceConfig.GetFileName(DeviceNum)),
+                out string errMsg))
+            {
+                throw new ScadaException(errMsg);
+            }
+
+            // create channels for subscriptions
+            List<CnlPrototype> cnlPrototypes = new();
+            int eventMask = new EventMask { Enabled = true, StatusChange = true, Command = true }.Value;
+            int cmdEventMask = new EventMask { Enabled = true, Command = true }.Value;
+
+            foreach (SubscriptionConfig subscriptionConfig in config.Subscriptions)
+            {
+                if (subscriptionConfig.JsEnabled && subscriptionConfig.SubItems.Count > 0)
+                {
+                    for (int i = 0, cnt = subscriptionConfig.SubItems.Count; i < cnt; i++)
+                    {
+                        string suffix = "." + subscriptionConfig.SubItems[i];
+                        cnlPrototypes.Add(new CnlPrototype
+                        {
+                            Name = subscriptionConfig.DisplayName + suffix,
+                            CnlTypeID = CnlTypeID.Input,
+                            TagCode = subscriptionConfig.TagCode + suffix,
+                            EventMask = eventMask
+                        });
+                    }
+                }
+                else
+                {
+                    cnlPrototypes.Add(new CnlPrototype
+                    {
+                        Name = subscriptionConfig.DisplayName,
+                        CnlTypeID = subscriptionConfig.ReadOnly ? CnlTypeID.Input : CnlTypeID.InputOutput,
+                        TagCode = subscriptionConfig.TagCode,
+                        EventMask = eventMask
+                    });
+                }
+            }
+
+            // create channels for commands
+            foreach (CommandConfig commandConfig in config.Commands)
+            {
+                cnlPrototypes.Add(new CnlPrototype
+                {
+                    Name = commandConfig.DisplayName,
+                    CnlTypeID = CnlTypeID.Output,
+                    TagCode = commandConfig.CmdCode,
+                    EventMask = cmdEventMask
+                });
+            }
+
+            return cnlPrototypes;
         }
     }
 }
