3bea16d3f8af652846e0ebf8966fce68641ec9b6
2mik <m_shir@mail.ru>
2022-09-29 12:31:50 +0300

    PlgMain: data report args form

---
+++ ScadaWeb/OpenPlugins/PlgMain/Areas/Main/Pages/HistDataReport.cshtml.cs
@@ -0,0 +1,19 @@
+// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Scada.Web.Plugins.PlgMain.Areas.Main.Pages
+{
+    /// <summary>
+    /// Represents a page for entering arguments for a historical data report.
+    /// <para>Представляет страницу для ввода аргументов отчёта по историческим данным.</para>
+    /// </summary>
+    public class HistDataReportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ScadaWeb/OpenPlugins/PlgMain/wwwroot/plugins/Main/js/hist-data-report.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $("#btnGenerateReport").click(function () {
+        if ($("#frmReportArgs")[0].reportValidity()) {
+            return false;
+        }
+    });
+});
