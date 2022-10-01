7052620e22b0e8439d7696628946cd0ce637a4de
2mik <m_shir@mail.ru>
2022-10-01 17:03:46 +0300

    PlgMain: event report form

--- ScadaWeb/OpenPlugins/PlgMain/Areas/Main/Pages/EventReport.cshtml.cs
+++ ScadaWeb/OpenPlugins/PlgMain/Areas/Main/Pages/EventReport.cshtml.cs
@@ -4,7 +4,9 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Scada.Data.Entities;
+using Scada.Lang;
 using Scada.Web.Services;
+using Scada.Web.Users;
 
 namespace Scada.Web.Plugins.PlgMain.Areas.Main.Pages
 {
@@ -16,17 +18,21 @@ namespace Scada.Web.Plugins.PlgMain.Areas.Main.Pages
     {
         private readonly IWebContext webContext;
         private readonly IUserContext userContext;
+        private readonly dynamic dict;
 
         public EventReportModel(IWebContext webContext, IUserContext userContext)
         {
             this.webContext = webContext;
             this.userContext = userContext;
+            dict = Locale.GetDictionary("Scada.Web.Plugins.PlgMain.Areas.Main.Pages.EventReport");
         }
 
         public DateTime StartTime { get; private set; }
         public DateTime EndTime { get; private set; }
         public string ArchiveCode { get; private set; } = null;
         public List<SelectListItem> ArchiveList { get; private set; } = new();
+        public List<SelectListItem> ObjList { get; private set; } = new();
+        public List<SelectListItem> SeverityList { get; private set; } = new();
 
 
         private void FillArchiveList()
@@ -40,11 +46,30 @@ namespace Scada.Web.Plugins.PlgMain.Areas.Main.Pages
             }
         }
 
+        private void FillObjList()
+        {
+            ObjList.Add(new SelectListItem(dict.AllObjectsItem, "0"));
+
+            foreach (ObjectItem objectItem in userContext.Objects)
+            {
+                ObjList.Add(new SelectListItem(
+                    objectItem.Text,
+                    objectItem.ObjNum.ToString()));
+            }
+        }
+
+        private void FillSeverityList()
+        {
+            SeverityList.Add(new SelectListItem(dict.AnySeverity, "0"));
+        }
+
         public void OnGet()
         {
             StartTime = userContext.ConvertTimeFromUtc(DateTime.UtcNow).Date;
             EndTime = StartTime.AddDays(1.0);
             FillArchiveList();
+            FillObjList();
+            FillSeverityList();
         }
     }
 }
