913e100621ccf32cfc0043657e6e0af005e95d1b
2mik <m_shir@mail.ru>
2022-10-24 14:57:31 +0300

    PlgSchBasicComp: add view project

---
+++ ScadaWeb/OpenPlugins/PlgSchBasicComp.View/PlgSchemeView.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Lang;
+
+namespace Scada.Web.Plugins.PlgSchBasicComp.View
+{
+    /// <summary>
+    /// Implements the plugin user interface for the Administrator application.
+    /// <para>Реализует пользовательский интерфейс плагина для приложения Администратор.</para>
+    /// </summary>
+    public class PlgSchBasicCompView : PluginView
+    {
+        /// <summary>
+        /// Gets the module name.
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return Locale.IsRussian ?
+                    "Основные компоненты схем" :
+                    "Basic Scheme Components";
+            }
+        }
+
+        /// <summary>
+        /// Gets the module description.
+        /// </summary>
+        public override string Descr
+        {
+            get
+            {
+                return Locale.IsRussian ?
+                    "Набор основных компонентов для отображения на мнемосхемах." :
+                    "A set of basic components for display on schemes.";
+            }
+        }
+    }
+}
