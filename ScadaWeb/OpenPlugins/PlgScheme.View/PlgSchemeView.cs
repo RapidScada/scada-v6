c6ba5301b1c2500a508dcc38c2f5f472ed86f7c4
2mik <m_shir@mail.ru>
2022-10-24 11:40:25 +0300

    PlgScheme: add view project

---
+++ ScadaWeb/OpenPlugins/PlgScheme.View/PlgSchemeView.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Lang;
+
+namespace Scada.Web.Plugins.PlgScheme.View
+{
+    /// <summary>
+    /// Implements the plugin user interface for the Administrator application.
+    /// <para>Реализует пользовательский интерфейс плагина для приложения Администратор.</para>
+    /// </summary>
+    public class PlgSchemeView : PluginView
+    {
+        /// <summary>
+        /// Gets the module name.
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return Locale.IsRussian ?
+                    "Схемы" :
+                    "Schemes";
+            }
+        }
+
+        /// <summary>
+        /// Gets the module description.
+        /// </summary>
+        public override string Descr
+        {
+            get
+            {
+                return Locale.IsRussian ?
+                    "Плагин обеспечивает отображение мнемосхем." :
+                    "The plugin provides displaying schemes.";
+            }
+        }
+    }
+}
