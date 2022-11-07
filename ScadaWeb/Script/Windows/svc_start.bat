f6d185a73ed3e92f91c0f5190bc7fee7ff6b4320
2mik <m_shir@mail.ru>
2022-11-07 13:09:56 +0300

    ScadaWeb: fix bat scripts

--- ScadaWeb/Script/Windows/svc_start.bat
+++ ScadaWeb/Script/Windows/svc_start.bat
@@ -1,5 +1,5 @@
 net start w3svc
 
-# Alternative
-#"%windir%\system32\inetsrv\appcmd" start SITE /site.name:Scada
-#"%windir%\system32\inetsrv\appcmd" start APPPOOL "ScadaAppPool"
+:: Alternative
+:: "%windir%\system32\inetsrv\appcmd" start SITE /site.name:Scada
+:: "%windir%\system32\inetsrv\appcmd" start APPPOOL "ScadaAppPool"
