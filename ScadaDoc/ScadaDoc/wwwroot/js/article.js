5ce1784066244043562ea0e1823c0bbe487c18c0
2mik <m_shir@mail.ru>
2022-11-19 12:58:48 +0300

    ScadaDoc: add jquery

---
+++ ScadaDoc/ScadaDoc/wwwroot/js/article.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    // scroll to the active item of the table of contents
+    let tocWrapper = $("#divTocWrapper");
+    let activeItem = tocWrapper.find(".item-text.active:first");
+
+    if (activeItem.length > 0) {
+        //tocWrapper.scrollTop(activeItem.offset().top);
+        //activeItem[0].scrollIntoView();
+    }
+});
