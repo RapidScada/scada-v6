74d5eb1ad501e379a4f2a70c90ce2e34c9ac32e2
2mik <m_shir@mail.ru>
2022-12-06 12:24:51 +0300

    ModActiveDirectory: dev

--- ScadaServer/OpenModules/ModActiveDirectory.Logic/ModActiveDirectoryLogic.cs
+++ ScadaServer/OpenModules/ModActiveDirectory.Logic/ModActiveDirectoryLogic.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright (c) Rapid Software LLC. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Scada.Data.Entities;
 using Scada.Server.Lang;
 using Scada.Server.Modules.ModActiveDirectory.Config;
+using System.DirectoryServices.Protocols;
+using System.Net;
 
 namespace Scada.Server.Modules.ModActiveDirectory.Logic
 {
@@ -12,7 +15,13 @@ namespace Scada.Server.Modules.ModActiveDirectory.Logic
     /// </summary>
     public class ModActiveDirectoryLogic : ModuleLogic
     {
-        private readonly ModuleConfig moduleConfig; // the module configuration
+        /// <summary>
+        /// The LDAP port.
+        /// </summary>
+        private const int LdapPort = 389;
+
+        private readonly ModuleConfig moduleConfig;      // the module configuration
+        private readonly Dictionary<string, User> users; // the users accessed by username
 
 
         /// <summary>
@@ -36,13 +45,180 @@ namespace Scada.Server.Modules.ModActiveDirectory.Logic
             }
         }
 
+
+        /*
+        /// <summary>
+        /// Finds the security groups in Active Directory that the specified group belongs to and adds them to the list.
+        /// </summary>
+        private static void FindOwnerGroups(DirectoryEntry entry, string group, List<string> groups)
+        {
+            DirectorySearcher search = new DirectorySearcher(entry);
+            search.Filter = "(distinguishedName=" + group + ")";
+            search.PropertiesToLoad.Add("memberOf");
+            SearchResult searchRes = search.FindOne();
+
+            if (searchRes != null)
+            {
+                foreach (object result in searchRes.Properties["memberOf"])
+                {
+                    string gr = result.ToString();
+                    groups.Add(gr);
+                    FindOwnerGroups(entry, gr, groups);
+                }
+            }
+        }
+        */
+        /// <summary>
+        /// Checks if the list of security groups contains the specified user role.
+        /// </summary>
+        private static bool GroupsContain(List<string> groups, string roleName)
+        {
+            roleName = "CN=" + roleName;
+
+            foreach (string group in groups)
+            {
+                if (group.StartsWith(roleName, StringComparison.OrdinalIgnoreCase))
+                    return true;
+            }
+
+            return false;
+        }
+
+
         /// <summary>
         /// Performs actions when starting the service.
         /// </summary>
         public override void OnServiceStart()
         {
+            // load module configuration
             if (!moduleConfig.Load(ServerContext.Storage, ModuleConfig.DefaultFileName, out string errMsg))
                 Log.WriteError(ServerPhrases.ModuleMessage, Code, errMsg);
+
+            // get users
+            foreach (User user in ServerContext.ConfigDatabase.UserTable)
+            {
+                if (!string.IsNullOrEmpty(user.Name))
+                    users[user.Name.Trim().ToLowerInvariant()] = user;
+            }
+        }
+
+        /// <summary>
+        /// Validates the username and password.
+        /// </summary>
+        public override bool ValidateUser(string username, string password,
+            out int userID, out int roleID, out string errMsg, out bool handled)
+        {
+            // https://github.com/dotnet/runtime/tree/main/src/libraries/System.DirectoryServices/src/System/DirectoryServices
+            // https://github.com/dotnet/runtime/issues/36888
+            // https://github.com/dotnet/runtime/issues/64900
+            // https://learn.microsoft.com/en-us/previous-versions/windows/desktop/dacx/how-to-enumerate-active-directory-definitions-for-user-and-device-claims-and-resource-properties
+
+            try
+            {
+                using LdapConnection connection = new(
+                    new LdapDirectoryIdentifier(moduleConfig.LdapPath, LdapPort, false, false),
+                    new NetworkCredential(username, password),
+                    AuthType.Ntlm);
+
+                connection.Bind();
+
+                string filter = $"(|(mail=joao.silva@brunobrito.net)(sAMAccountName={username}))";
+                SearchRequest request = new("DC=brunobrito,DC=net", filter, SearchScope.Subtree, "displayName", "cn", "mail");
+                DirectoryResponse response = connection.SendRequest(request); // response is SearchResponse
+            }
+            catch (Exception ex)
+            {
+                Log.WriteError(ServerPhrases.ModuleMessage, Code, ex.Message);
+            }
+
+            /*DirectoryEntry entry = null;
+
+            try
+            {
+                // check password
+                bool pwdOK = false;
+
+                if (string.IsNullOrEmpty(password))
+                {
+                    entry = new DirectoryEntry(moduleConfig.LdapPath);
+                    pwdOK = true;
+                }
+                else
+                {
+                    entry = new DirectoryEntry(moduleConfig.LdapPath, username, password);
+
+                    // user authentication
+                    try
+                    {
+                        object native = entry.NativeObject;
+                        pwdOK = true;
+                    }
+                    catch { }
+                }
+
+                if (pwdOK)
+                {
+                    if (users.TryGetValue(username.Trim().ToLowerInvariant(), out User user))
+                    {
+                        roleID = user.RoleID;
+                        handled = true;
+                        return true;
+                    }
+                    else
+                    {
+                        // get user security groups
+                        DirectorySearcher search = new DirectorySearcher(entry);
+                        search.Filter = "(sAMAccountName=" + username + ")";
+                        search.PropertiesToLoad.Add("memberOf");
+                        SearchResult searchRes = search.FindOne();
+
+                        if (searchRes != null)
+                        {
+                            List<string> groups = new List<string>();
+                            foreach (object result in searchRes.Properties["memberOf"])
+                            {
+                                string group = result.ToString();
+                                groups.Add(group);
+                                FindOwnerGroups(entry, group, groups);
+                            }
+
+                            // define user role
+                            if (GroupsContain(groups, "ScadaDisabled"))
+                                roleID = BaseValues.Roles.Disabled;
+                            else if (GroupsContain(groups, "ScadaGuest"))
+                                roleID = BaseValues.Roles.Guest;
+                            else if (GroupsContain(groups, "ScadaDispatcher"))
+                                roleID = BaseValues.Roles.Dispatcher;
+                            else if (GroupsContain(groups, "ScadaAdmin"))
+                                roleID = BaseValues.Roles.Admin;
+                            else if (GroupsContain(groups, "ScadaApp"))
+                                roleID = BaseValues.Roles.App;
+                            else
+                                roleID = BaseValues.Roles.Err;
+
+                            // return successful result
+                            if (roleID != BaseValues.Roles.Err)
+                            {
+                                handled = true;
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteToLog(string.Format(Localization.UseRussian ?
+                    "{0}. Ошибка при работе с Active Directory: {1}" :
+                    "{0}. Error working with Active Directory: {1}", Name, ex.Message),
+                    Log.ActTypes.Exception);
+            }
+            finally
+            {
+                entry?.Close();
+            }*/
+
+            return base.ValidateUser(username, password, out userID, out roleID, out errMsg, out handled);
         }
     }
 }
--- ScadaServer/OpenModules/ModActiveDirectory.View/ModActiveDirectoryView.cs
+++ ScadaServer/OpenModules/ModActiveDirectory.View/ModActiveDirectoryView.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Lang;
 
 namespace Scada.Server.Modules.ModActiveDirectory.View
 {
-    public class ModActiveDirectoryView
+    /// <summary>
+    /// Implements the server module user interface.
+    /// <para>Реализует пользовательский интерфейс серверного модуля.</para>
+    /// </summary>
+    public class ModActiveDirectoryView : ModuleView
     {
+        /// <summary>
+        /// Gets the module name.
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return Locale.IsRussian ?
+                    "Аутентификация Active Directory" :
+                    "Active Directory Authentication";
+            }
+        }
+
+        /// <summary>
+        /// Gets the module description.
+        /// </summary>
+        public override string Descr
+        {
+            get
+            {
+                return Locale.IsRussian ?
+                    "Модуль проверяет имя и пароль пользователя, используя Active Directory." :
+                    "The module validates username and password using Active Directory.";
+            }
+        }
     }
 }
