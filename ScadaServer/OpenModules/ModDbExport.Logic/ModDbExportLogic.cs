8b2cd31d2008a294b2f78d871a9ddcb13f02a917
2mik <m_shir@mail.ru>
2022-12-08 17:24:11 +0300

    ModDbExport: project structure

---
+++ ScadaServer/OpenModules/ModDbExport.Logic/ModDbExportLogic.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Scada.Server.Modules.ModDbExport.Logic
+{
+    /// <summary>
+    /// Implements the server module logic.
+    /// <para>Реализует логику серверного модуля.</para>
+    /// </summary>
+    public class ModDbExportLogic : ModuleLogic
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public ModDbExportLogic(IServerContext serverContext)
+            : base(serverContext)
+        {
+        }
+
+
+        /// <summary>
+        /// Gets the module code.
+        /// </summary>
+        public override string Code
+        {
+            get
+            {
+                return ModuleUtils.ModuleCode;
+            }
+        }
+    }
+}
---
+++ ScadaServer/OpenModules/ModDbExport.View/ModDbExportView.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Lang;
+
+namespace Scada.Server.Modules.ModDbExport.View
+{
+    /// <summary>
+    /// Implements the server module user interface.
+    /// <para>Реализует пользовательский интерфейс серверного модуля.</para>
+    /// </summary>
+    public class ModDbExportView : ModuleView
+    {
+        /// <summary>
+        /// Gets the module name.
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return Locale.IsRussian ?
+                    "Экпорт в БД" :
+                    "Database Export";
+            }
+        }
+
+        /// <summary>
+        /// Gets the module description.
+        /// </summary>
+        public override string Descr
+        {
+            get
+            {
+                return Locale.IsRussian ?
+                    "Модуль экспортирует данные в сторонние базы данных. " + 
+                    "Поддерживает Microsoft SQL Server, Oracle, PostgreSQL и MySQL." :
+                    "The module exports data to external databases. " + 
+                    "Supports Microsoft SQL Server, Oracle, PostgreSQL and MySQL.";
+            }
+        }
+    }
+}
