fef097610951e0d85c8383f74e8002dc2600ea79
2mik <m_shir@mail.ru>
2022-12-13 13:31:13 +0300

    ScadaAdmin: update version

--- ScadaAdmin/ScadaAdmin/ScadaAdminCommon/AdminUtils.cs
+++ ScadaAdmin/ScadaAdmin/ScadaAdminCommon/AdminUtils.cs
@@ -20,10 +20,11 @@
  * 
  * Author   : Mikhail Shiryaev
  * Created  : 2018
- * Modified : 2021
+ * Modified : 2022
  */
 
 using System.IO;
+using System.Reflection;
 
 namespace Scada.Admin
 {
@@ -36,7 +37,7 @@ namespace Scada.Admin
         /// <summary>
         /// The application version.
         /// </summary>
-        public const string AppVersion = "6.0.0.0";
+        public static readonly string AppVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
         /// <summary>
         /// The application log file name.
         /// </summary>
