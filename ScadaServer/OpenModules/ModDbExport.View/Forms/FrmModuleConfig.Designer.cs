9e9f59e8fb20b4c349793dc5c6772d2024e92f26
greenfoxx <elena@rapidscada.ru>
2022-12-20 10:20:24 +0300

    ModDbExport: Add new forms

---
+++ ScadaServer/OpenModules/ModDbExport.View/Forms/FrmModuleConfig.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Scada.Server.Modules.ModDbExport.View.Forms
+{
+    partial class FrmModuleConfig
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlBottom = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnMoveUp = new System.Windows.Forms.ToolStripButton();
+            this.btnMoveDown = new System.Windows.Forms.ToolStripButton();
+            this.btnDelete = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnCut = new System.Windows.Forms.ToolStripButton();
+            this.btnCopy = new System.Windows.Forms.ToolStripButton();
+            this.btnPaste = new System.Windows.Forms.ToolStripButton();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.pnlTree = new System.Windows.Forms.Panel();
+            this.pnlInfo = new System.Windows.Forms.Panel();
+            this.gbTarget = new System.Windows.Forms.GroupBox();
+            this.tvTargets = new System.Windows.Forms.TreeView();
+            this.ddbAdd = new System.Windows.Forms.ToolStripDropDownButton();
+            this.pnlBottom.SuspendLayout();
+            this.toolStrip.SuspendLayout();
+            this.pnlMain.SuspendLayout();
+            this.tableLayoutPanel.SuspendLayout();
+            this.pnlTree.SuspendLayout();
+            this.gbTarget.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlBottom
+            // 
+            this.pnlBottom.Controls.Add(this.btnClose);
+            this.pnlBottom.Controls.Add(this.btnCancel);
+            this.pnlBottom.Controls.Add(this.btnSave);
+            this.pnlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlBottom.Location = new System.Drawing.Point(0, 496);
+            this.pnlBottom.Name = "pnlBottom";
+            this.pnlBottom.Size = new System.Drawing.Size(734, 45);
+            this.pnlBottom.TabIndex = 3;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(647, 10);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(566, 10);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(485, 10);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ddbAdd,
+            this.toolStripSeparator1,
+            this.btnMoveUp,
+            this.btnMoveDown,
+            this.btnDelete,
+            this.toolStripSeparator2,
+            this.btnCut,
+            this.btnCopy,
+            this.btnPaste});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(734, 25);
+            this.toolStrip.TabIndex = 5;
+            this.toolStrip.Text = "Add export target";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnMoveUp
+            // 
+            this.btnMoveUp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnMoveUp.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnMoveUp.Name = "btnMoveUp";
+            this.btnMoveUp.Size = new System.Drawing.Size(23, 22);
+            this.btnMoveUp.Text = "Move Up";
+            this.btnMoveUp.ToolTipText = "Move Up";
+            // 
+            // btnMoveDown
+            // 
+            this.btnMoveDown.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnMoveDown.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnMoveDown.Name = "btnMoveDown";
+            this.btnMoveDown.Size = new System.Drawing.Size(23, 22);
+            this.btnMoveDown.Text = "Move Down";
+            this.btnMoveDown.ToolTipText = "Move Down";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(23, 22);
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.ToolTipText = "Delete";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnCut
+            // 
+            this.btnCut.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnCut.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnCut.Name = "btnCut";
+            this.btnCut.Size = new System.Drawing.Size(23, 22);
+            this.btnCut.Text = "Cut";
+            this.btnCut.ToolTipText = "Cut";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnCopy.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(23, 22);
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.ToolTipText = "Copy";
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnPaste.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(23, 22);
+            this.btnPaste.Text = "Paste";
+            this.btnPaste.ToolTipText = "Paste";
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Controls.Add(this.tableLayoutPanel);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 25);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(734, 471);
+            this.pnlMain.TabIndex = 6;
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 42.91553F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 57.08447F));
+            this.tableLayoutPanel.Controls.Add(this.pnlTree, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.pnlInfo, 1, 0);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 1;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(734, 471);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // pnlTree
+            // 
+            this.pnlTree.Controls.Add(this.gbTarget);
+            this.pnlTree.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlTree.Location = new System.Drawing.Point(0, 0);
+            this.pnlTree.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlTree.Name = "pnlTree";
+            this.pnlTree.Size = new System.Drawing.Size(315, 471);
+            this.pnlTree.TabIndex = 0;
+            // 
+            // pnlInfo
+            // 
+            this.pnlInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlInfo.Location = new System.Drawing.Point(315, 0);
+            this.pnlInfo.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlInfo.Name = "pnlInfo";
+            this.pnlInfo.Size = new System.Drawing.Size(419, 471);
+            this.pnlInfo.TabIndex = 1;
+            // 
+            // gbTarget
+            // 
+            this.gbTarget.Controls.Add(this.tvTargets);
+            this.gbTarget.Location = new System.Drawing.Point(12, 3);
+            this.gbTarget.Name = "gbTarget";
+            this.gbTarget.Padding = new System.Windows.Forms.Padding(10, 3, 10, 10);
+            this.gbTarget.Size = new System.Drawing.Size(300, 462);
+            this.gbTarget.TabIndex = 1;
+            this.gbTarget.TabStop = false;
+            this.gbTarget.Text = "Export Targets";
+            // 
+            // tvTargets
+            // 
+            this.tvTargets.HideSelection = false;
+            this.tvTargets.Location = new System.Drawing.Point(13, 22);
+            this.tvTargets.Name = "tvTargets";
+            this.tvTargets.Size = new System.Drawing.Size(274, 427);
+            this.tvTargets.TabIndex = 0;
+            // 
+            // ddbAdd
+            // 
+            this.ddbAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ddbAdd.Image = global::Scada.Server.Modules.ModDbExport.View.Properties.Resources.add_db;
+            this.ddbAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ddbAdd.Name = "ddbAdd";
+            this.ddbAdd.Size = new System.Drawing.Size(29, 22);
+            this.ddbAdd.Text = "toolStripDropDownButton1";
+            // 
+            // FrmModuleConfig
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 541);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.toolStrip);
+            this.Controls.Add(this.pnlBottom);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmModuleConfig";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Export to DB";
+            this.pnlBottom.ResumeLayout(false);
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            this.pnlMain.ResumeLayout(false);
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.pnlTree.ResumeLayout(false);
+            this.gbTarget.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Panel pnlBottom;
+        private Button btnClose;
+        private Button btnCancel;
+        private Button btnSave;
+        private ToolStrip toolStrip;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripButton btnMoveUp;
+        private ToolStripButton btnMoveDown;
+        private ToolStripButton btnDelete;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripButton btnCut;
+        private ToolStripButton btnCopy;
+        private ToolStripButton btnPaste;
+        private Panel pnlMain;
+        private TableLayoutPanel tableLayoutPanel;
+        private Panel pnlTree;
+        private Panel pnlInfo;
+        private GroupBox gbTarget;
+        private TreeView tvTargets;
+        private ToolStripDropDownButton ddbAdd;
+    }
+}
---
+++ ScadaServer/OpenModules/ModDbExport.View/Forms/FrmModuleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scada.Server.Modules.ModDbExport.View.Forms
+{
+    public partial class FrmModuleConfig : Form
+    {
+        public FrmModuleConfig()
+        {
+            InitializeComponent();
+        }
+    }
+}
