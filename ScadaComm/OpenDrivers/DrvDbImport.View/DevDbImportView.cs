924029b5b12d92d39c43a025791fb7abbd4668aa
2mik <m_shir@mail.ru>
2023-01-11 14:05:35 +0300

    DrvDbImport: channel prototypes

--- ScadaComm/OpenDrivers/DrvDbImport.View/DevDbImportView.cs
+++ ScadaComm/OpenDrivers/DrvDbImport.View/DevDbImportView.cs
@@ -3,6 +3,9 @@
 
 using Scada.Comm.Config;
 using Scada.Comm.Devices;
+using Scada.Comm.Drivers.DrvDbImport.Config;
+using Scada.Data.Const;
+using Scada.Data.Models;
 
 namespace Scada.Comm.Drivers.DrvDbImport.View
 {
@@ -18,6 +21,16 @@ namespace Scada.Comm.Drivers.DrvDbImport.View
         public DevDbImportView(DriverView parentView, LineConfig lineConfig, DeviceConfig deviceConfig)
             : base(parentView, lineConfig, deviceConfig)
         {
+            CanShowProperties = false;
+        }
+
+
+        /// <summary>
+        /// Shows a modal dialog box for editing device properties.
+        /// </summary>
+        public override bool ShowProperties()
+        {
+            return false;
         }
 
         /// <summary>
@@ -26,6 +39,54 @@ namespace Scada.Comm.Drivers.DrvDbImport.View
         public override PollingOptions GetPollingOptions()
         {
             return PollingOptions.CreateWithDefaultDelay();
+        }
+
+        /// <summary>
+        /// Gets the channel prototypes for the device.
+        /// </summary>
+        public override ICollection<CnlPrototype> GetCnlPrototypes()
+        {
+            DbDeviceConfig config = new();
+
+            if (!config.Load(Path.Combine(AppDirs.ConfigDir, DbDeviceConfig.GetFileName(DeviceNum)),
+                out string errMsg))
+            {
+                throw new ScadaException(errMsg);
+            }
+
+            // create channel prototypes
+            List<CnlPrototype> cnlPrototypes = new();
+            int eventMask = new EventMask { Enabled = true, StatusChange = true }.Value;
+            int cmdEventMask = new EventMask { Enabled = true, Command = true }.Value;
+
+            // queues
+            foreach (QueryConfig queryConfig in config.Queries)
+            {
+                foreach (string tagCode in queryConfig.TagCodes)
+                {
+                    cnlPrototypes.Add(new CnlPrototype
+                    {
+                        Name = tagCode,
+                        CnlTypeID = CnlTypeID.Input,
+                        TagCode = tagCode,
+                        EventMask = eventMask
+                    });
+                }
+            }
+
+            // commands
+            foreach (CommandConfig commandConfig in config.Commands)
+            {
+                cnlPrototypes.Add(new CnlPrototype
+                {
+                    Name = commandConfig.Name,
+                    CnlTypeID = CnlTypeID.Output,
+                    TagCode = commandConfig.CmdCode,
+                    EventMask = cmdEventMask
+                });
+            }
+
+            return cnlPrototypes;
         }
     }
 }
