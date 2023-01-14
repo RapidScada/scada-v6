a35787bcb2b4809385c50337480b91cff4a0a235
2mik <m_shir@mail.ru>
2023-01-14 11:51:14 +0300

    DrvSnmp: channel prototypes

--- ScadaComm/OpenDrivers2/DrvSnmp.View/DevSnmpView.cs
+++ ScadaComm/OpenDrivers2/DrvSnmp.View/DevSnmpView.cs
@@ -3,6 +3,9 @@
 
 using Scada.Comm.Config;
 using Scada.Comm.Devices;
+using Scada.Comm.Drivers.DrvSnmp.Config;
+using Scada.Data.Const;
+using Scada.Data.Models;
 using Scada.Forms.Forms;
 
 namespace Scada.Comm.Drivers.DrvSnmp.View
@@ -22,6 +25,7 @@ namespace Scada.Comm.Drivers.DrvSnmp.View
             CanShowProperties = true;
         }
 
+
         /// <summary>
         /// Shows a modal dialog box for editing device properties.
         /// </summary>
@@ -29,6 +33,47 @@ namespace Scada.Comm.Drivers.DrvSnmp.View
         {
             new FrmModuleConfig(new SnmpConfigProvider(AppDirs.ConfigDir, DeviceNum)).ShowDialog();
             return false;
+        }
+
+        /// <summary>
+        /// Gets the channel prototypes for the device.
+        /// </summary>
+        public override ICollection<CnlPrototype> GetCnlPrototypes()
+        {
+            // load device configuration
+            SnmpDeviceConfig config = new();
+
+            if (!config.Load(Path.Combine(AppDirs.ConfigDir, SnmpDeviceConfig.GetFileName(DeviceNum)),
+                out string errMsg))
+            {
+                throw new ScadaException(errMsg);
+            }
+
+            // create channel prototypes
+            List<CnlPrototype> cnlPrototypes = new();
+            int eventMask = new EventMask { Enabled = true, StatusChange = true }.Value;
+
+            foreach (VarGroupConfig varGroupConfig in config.VarGroups)
+            {
+                foreach (VariableConfig variableConfig in varGroupConfig.Variables)
+                {
+                    int dataTypeID = (int)variableConfig.DataType;
+                    cnlPrototypes.Add(new CnlPrototype
+                    {
+                        Active = varGroupConfig.Active,
+                        Name = variableConfig.Name,
+                        DataTypeID = dataTypeID > 0 ? dataTypeID : null,
+                        DataLen = variableConfig.DataLen > 0 ? 
+                            DeviceTag.CalcDataLength(variableConfig.DataLen, variableConfig.DataType) : null,
+                        CnlTypeID = CnlTypeID.Input,
+                        TagCode = variableConfig.Name,
+                        EventMask = eventMask,
+                        FormatCode = DataTypeID.IsString(dataTypeID) ? FormatCode.String : null
+                    });
+                }
+            }
+
+            return cnlPrototypes;
         }
     }
 }
