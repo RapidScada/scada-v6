d292296677a0fe398a7fd7aefb619bc00ab00706
2mik <m_shir@mail.ru>
2023-01-24 18:43:20 +0300

    DrvSms: add logic classes

---
+++ ScadaComm/OpenDrivers2/DrvSms.Logic/DevSmsLogic.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Comm.Config;
+using Scada.Comm.Devices;
+
+namespace Scada.Comm.Drivers.DrvSms.Logic
+{
+    /// <summary>
+    /// Implements the device logic.
+    /// <para>Реализует логику устройства.</para>
+    /// </summary>
+    internal class DevSmsLogic : DeviceLogic
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public DevSmsLogic(ICommContext commContext, ILineContext lineContext, DeviceConfig deviceConfig)
+            : base(commContext, lineContext, deviceConfig)
+        {
+            CanSendCommands = true;
+        }
+    }
+}
--- ScadaComm/OpenDrivers2/DrvSms.Logic/DrvSmsLogic.cs
+++ ScadaComm/OpenDrivers2/DrvSms.Logic/DrvSmsLogic.cs
@@ -1,10 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Comm.Config;
+using Scada.Comm.Devices;
 
 namespace Scada.Comm.Drivers.DrvSms.Logic
 {
-    internal class DrvSmsLogic
+    /// <summary>
+    /// Implements the driver logic.
+    /// <para>Реализует логику драйвера.</para>
+    /// </summary>
+    internal class DrvSmsLogic : DriverLogic
     {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public DrvSmsLogic(ICommContext commContext)
+            : base(commContext)
+        {
+        }
+
+        /// <summary>
+        /// Gets the driver code.
+        /// </summary>
+        public override string Code
+        {
+            get
+            {
+                return DriverUtils.DriverCode;
+            }
+        }
+
+        /// <summary>
+        /// Creates a new device.
+        /// </summary>
+        public override DeviceLogic CreateDevice(ILineContext lineContext, DeviceConfig deviceConfig)
+        {
+            return new DevSmsLogic(CommContext, lineContext, deviceConfig);
+        }
     }
 }
