cf050053d13ff1b4eac55b760bea1be76112bdfe
2mik <m_shir@mail.ru>
2023-01-26 17:21:06 +0300

    DrvSms: add unit test project

---
+++ ScadaComm/OpenDrivers2/DrvSms.Tests/PduConverterTests.cs
@@ -0,0 +1,11 @@
+﻿namespace Scada.Comm.Drivers.DrvSms.Tests
+{
+    [TestClass]
+    public class PduConverterTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
