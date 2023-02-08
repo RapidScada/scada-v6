b06aaffd0256668b01bb7eb529b63a3f8a1e6c5e
2mik <m_shir@mail.ru>
2023-02-08 10:40:00 +0300

    Update CommUtils.cs

--- ScadaComm/ScadaComm/ScadaCommCommon/CommUtils.cs
+++ ScadaComm/ScadaComm/ScadaCommCommon/CommUtils.cs
@@ -20,7 +20,7 @@
  * 
  * Author   : Mikhail Shiryaev
  * Created  : 2015
- * Modified : 2022
+ * Modified : 2023
  */
 
 using Scada.Comm.Devices;
@@ -156,9 +156,9 @@ namespace Scada.Comm
         /// <summary>
         /// Gets a flatten list of the channel prototypes.
         /// </summary>
-        public static List<CnlPrototype> GetCnlPrototypes(this List<CnlPrototypeGroup> cnlPrototypeGroups)
+        public static List<CnlPrototype> GetCnlPrototypes(this List<CnlPrototypeGroup> groups)
         {
-            return cnlPrototypeGroups.SelectMany(group => group.CnlPrototypes).ToList();
+            return groups.SelectMany(group => group.CnlPrototypes).ToList();
         }
     }
 }
