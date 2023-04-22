77f4f3109caeefae7fd826fcf62e6f6cbf6e9f23
MG messie <moukmessie@gmail.com>
2023-04-22 10:49:26 +0200

    Feat(ExtImpForms): Add Cnl import form

---
+++ ScadaAdmin/OpenExtensions/ExtImport/Forms/FrmCnlImport.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Scada.Admin.Extensions.ExtImport.Forms
+{
+	partial class FrmCnlImport
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			SuspendLayout();
+			// 
+			// FrmCnlImport
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(458, 334);
+			Name = "FrmCnlImport";
+			Text = "FrmCnlImport";
+			ResumeLayout(false);
+		}
+
+		#endregion
+	}
+}
---
+++ ScadaAdmin/OpenExtensions/ExtImport/Forms/FrmCnlImport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scada.Admin.Extensions.ExtImport.Forms
+{
+	public partial class FrmCnlImport : Form
+	{
+		public FrmCnlImport()
+		{
+			InitializeComponent();
+		}
+	}
+}
