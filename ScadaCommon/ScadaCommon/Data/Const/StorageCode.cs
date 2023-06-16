a63bbc142dc0d8077105974fdb05d20c05aeccbf
2mik <m_shir@mail.ru>
2023-06-16 15:50:33 +0300

    Create StorageCode.cs

---
+++ ScadaCommon/ScadaCommon/Data/Const/StorageCode.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Copyright 2023 Rapid Software LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Product  : Rapid SCADA
+ * Module   : ScadaCommon
+ * Summary  : Specifies the storage codes
+ * 
+ * Author   : Mikhail Shiryaev
+ * Created  : 2023
+ * Modified : 2023
+ */
+
+#pragma warning disable 1591 // Missing XML comment for publicly visible type or member
+
+namespace Scada.Data.Const
+{
+    /// <summary>
+    /// Specifies the storage codes.
+    /// <para>Задаёт коды хранилищ.</para>
+    /// </summary>
+    public static class StorageCode
+    {
+        public const string FileStorage = nameof(FileStorage);
+        public const string PostgreSqlStorage = nameof(PostgreSqlStorage);
+    }
+}
