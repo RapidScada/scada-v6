76db8e3d3295a5d93f2af79898b172d1cc70d0a2
Anaïs THORAL <50917972+anaisthrl@users.noreply.github.com>
2023-07-07 14:51:57 +0200

    fit: ExtBitReader creation

---
+++ ScadaAdmin/OpenExtensions/ExtBitReader/ExtBitReaderLogic.cs
@@ -0,0 +1,37 @@
+﻿using Scada.Admin.Lang;
+using Scada.Forms;
+using Scada.Lang;
+
+namespace Scada.Admin.Extensions.ExtBitReader
+{
+    public class ExtBitReaderLogic : ExtensionLogic
+    {
+        public ExtBitReaderLogic(IAdminContext adminContext) : base(adminContext)
+        {
+        }
+
+        public override string Code
+        {
+            get
+            {
+                return "ExtBitReader";
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Bit Reader Extension";
+            }
+        }
+
+        public override string Descr
+        {
+            get
+            {
+                return "The extension allows for the reading and the seperation of bits.";
+            }
+        }
+    }
+}
