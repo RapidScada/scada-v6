23acc9df895ee276923d26a8704cc45b84b41e61
MG messie <moukmessie@gmail.com>
2023-07-29 22:50:53 +0200

    Revert: remove of DynamicPolygon and StaticPolygon components

--- ScadaWeb/OpenPlugins/PlgScheme.Common/CompManager.cs
+++ ScadaWeb/OpenPlugins/PlgScheme.Common/CompManager.cs
@@ -45,8 +45,7 @@ namespace Scada.Web.Plugins.PlgScheme
                 { typeof(DynamicText).FullName, typeof(DynamicText) },
                 { typeof(StaticPicture).FullName, typeof(StaticPicture) },
                 { typeof(DynamicPicture).FullName, typeof(DynamicPicture) },
-                { typeof(StaticPolygon).FullName, typeof(StaticPolygon) },
-                { typeof(DynamicPolygon).FullName, typeof(DynamicPolygon) }
+              
             };
         }
 
@@ -75,9 +74,8 @@ namespace Scada.Web.Plugins.PlgScheme
         /// </summary>
         private static bool TypeIsStrandard(Type compType)
         {
-            return compType == typeof(StaticText) || compType == typeof(DynamicText) || 
-                compType == typeof(StaticPicture) || compType == typeof(DynamicPicture) || 
-                compType == typeof(StaticPolygon) || compType == typeof(DynamicPolygon);
+            return compType == typeof(StaticText) || compType == typeof(DynamicText) ||
+                compType == typeof(StaticPicture) || compType == typeof(DynamicPicture);
         }
 
         /// <summary>
@@ -216,10 +214,6 @@ namespace Scada.Web.Plugins.PlgScheme
                             return new StaticPicture();
                         else if (localName == "dynamicpicture")
                             return new DynamicPicture();
-						else if (localName == "staticpolygon")
-							return new StaticPolygon();
-						else if (localName == "dynamicpolygon")
-							return new DynamicPolygon();
 						else
                             errMsg = string.Format(SchemePhrases.UnknownComponent, nodeName);
                     }
