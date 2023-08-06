9efd19a1e43b6c75fb6ea1c6210bcf0675e39108
2mik <m_shir@mail.ru>
2023-08-06 14:34:28 +0300

    Initial commit of PlgScheme.EditorWeb

---
+++ ScadaWeb/OpenPlugins/PlgScheme.EditorWeb/Program.cs
@@ -0,0 +1,15 @@
+namespace PlgScheme.EditorWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
