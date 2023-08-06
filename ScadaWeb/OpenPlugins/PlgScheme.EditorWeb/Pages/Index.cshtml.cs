0416bf9605dec678aa034f4ba0fb8bc427d84b44
2mik <m_shir@mail.ru>
2023-08-06 16:01:16 +0300

    PlgScheme.EditorWeb: add client libs

---
+++ ScadaWeb/OpenPlugins/PlgScheme.EditorWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Scada.Web.Plugins.PlgScheme.Editor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- ScadaWeb/OpenPlugins/PlgScheme.EditorWeb/Program.cs
+++ ScadaWeb/OpenPlugins/PlgScheme.EditorWeb/Program.cs
@@ -1,14 +1,25 @@
+// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 namespace PlgScheme.EditorWeb
 {
+    /// <summary>
+    /// The Scheme Editor web application.
+    /// <para>Веб-приложение Редактор схем.</para>
+    /// </summary>
     public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddRazorPages();
+            builder.Services.AddControllers();
+
             var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World!");
-
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapRazorPages();
+            app.MapControllers();
             app.Run();
         }
     }
