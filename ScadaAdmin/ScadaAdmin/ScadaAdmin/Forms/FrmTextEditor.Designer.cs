4dc77f05236bb6dfb463aba208296a75e29d1ff3
2mik <m_shir@mail.ru>
2023-09-13 10:51:48 +0300

    ScadaAdmin: update tooltip

--- ScadaAdmin/ScadaAdmin/ScadaAdmin/Forms/FrmTextEditor.Designer.cs
+++ ScadaAdmin/ScadaAdmin/ScadaAdmin/Forms/FrmTextEditor.Designer.cs
@@ -28,60 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox = new System.Windows.Forms.RichTextBox();
-            this.toolStrip = new System.Windows.Forms.ToolStrip();
-            this.btnReload = new System.Windows.Forms.ToolStripButton();
-            this.toolStrip.SuspendLayout();
-            this.SuspendLayout();
+            richTextBox = new System.Windows.Forms.RichTextBox();
+            toolStrip = new System.Windows.Forms.ToolStrip();
+            btnReload = new System.Windows.Forms.ToolStripButton();
+            toolStrip.SuspendLayout();
+            SuspendLayout();
             // 
             // richTextBox
             // 
-            this.richTextBox.AcceptsTab = true;
-            this.richTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.richTextBox.Location = new System.Drawing.Point(0, 25);
-            this.richTextBox.Name = "richTextBox";
-            this.richTextBox.Size = new System.Drawing.Size(584, 336);
-            this.richTextBox.TabIndex = 0;
-            this.richTextBox.Text = "";
-            this.richTextBox.WordWrap = false;
-            this.richTextBox.TextChanged += new System.EventHandler(this.richTextBox_TextChanged);
+            richTextBox.AcceptsTab = true;
+            richTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            richTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            richTextBox.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            richTextBox.Location = new System.Drawing.Point(0, 25);
+            richTextBox.Name = "richTextBox";
+            richTextBox.Size = new System.Drawing.Size(584, 336);
+            richTextBox.TabIndex = 0;
+            richTextBox.Text = "";
+            richTextBox.WordWrap = false;
+            richTextBox.TextChanged += richTextBox_TextChanged;
             // 
             // toolStrip
             // 
-            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.btnReload});
-            this.toolStrip.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip.Name = "toolStrip";
-            this.toolStrip.Size = new System.Drawing.Size(584, 25);
-            this.toolStrip.TabIndex = 1;
+            toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { btnReload });
+            toolStrip.Location = new System.Drawing.Point(0, 0);
+            toolStrip.Name = "toolStrip";
+            toolStrip.Size = new System.Drawing.Size(584, 25);
+            toolStrip.TabIndex = 1;
             // 
             // btnReload
             // 
-            this.btnReload.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnReload.Image = global::Scada.Admin.App.Properties.Resources.refresh;
-            this.btnReload.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnReload.Name = "btnReload";
-            this.btnReload.Size = new System.Drawing.Size(23, 22);
-            this.btnReload.ToolTipText = "Reload file";
-            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            btnReload.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            btnReload.Image = Properties.Resources.refresh;
+            btnReload.ImageTransparentColor = System.Drawing.Color.Magenta;
+            btnReload.Name = "btnReload";
+            btnReload.Size = new System.Drawing.Size(23, 22);
+            btnReload.ToolTipText = "Reload File";
+            btnReload.Click += btnReload_Click;
             // 
             // FrmTextEditor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 361);
-            this.Controls.Add(this.richTextBox);
-            this.Controls.Add(this.toolStrip);
-            this.Name = "FrmTextEditor";
-            this.Text = "FrmTextEditor";
-            this.Load += new System.EventHandler(this.FrmTextEditor_Load);
-            this.toolStrip.ResumeLayout(false);
-            this.toolStrip.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(584, 361);
+            Controls.Add(richTextBox);
+            Controls.Add(toolStrip);
+            Name = "FrmTextEditor";
+            Text = "FrmTextEditor";
+            Load += FrmTextEditor_Load;
+            toolStrip.ResumeLayout(false);
+            toolStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
