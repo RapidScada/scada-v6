5b7dd5e55064776ec140a56dc57383f5321304e0
2mik <m_shir@mail.ru>
2023-11-04 14:24:36 +0300

    Initial commit of DrvCsvReader

---
+++ ScadaComm/OpenDrivers2/DrvCsvReader.Shared/CsvReaderOptions.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace Scada.Comm.Drivers.DrvCsvReader
+{
+    /// <summary>
+    /// Represents CSV reader options.
+    /// <para>Представляет параметры считывателя CSV.</para>
+    /// </summary>
+    internal class CsvReaderOptions
+    {
+        [Description("The name of the CSV file containing data to read.")]
+        public string FileName { get; set; }
+
+        [Description("The string used as the decimal separator in numeric values.")]
+        public string DecimalSeparator { get; set; } = ".";
+
+        [Description("The delimiter used to separate fields.")]
+        public string FieldDelimiter { get; set; } = ",";
+    }
+}
