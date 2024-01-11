3ec46faff9f32936a01d47c894dc4ffc7c561314
2mik <m_shir@mail.ru>
2024-01-11 17:36:02 +0300

    ScadaCommon: refresh forms

--- ScadaCommon/ScadaCommon.Forms/Forms/FrmRangeEdit.Designer.cs
+++ ScadaCommon/ScadaCommon.Forms/Forms/FrmRangeEdit.Designer.cs
@@ -28,91 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnCancel = new System.Windows.Forms.Button();
-            this.btnOK = new System.Windows.Forms.Button();
-            this.lblRange = new System.Windows.Forms.Label();
-            this.txtRange = new System.Windows.Forms.TextBox();
-            this.lblExample = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            btnCancel = new Button();
+            btnOK = new Button();
+            lblRange = new Label();
+            txtRange = new TextBox();
+            lblExample = new Label();
+            SuspendLayout();
             // 
             // btnCancel
             // 
-            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(247, 176);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
-            this.btnCancel.TabIndex = 4;
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnCancel.DialogResult = DialogResult.Cancel;
+            btnCancel.Location = new Point(247, 176);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 23);
+            btnCancel.TabIndex = 4;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
             // 
             // btnOK
             // 
-            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnOK.Location = new System.Drawing.Point(166, 176);
-            this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(75, 23);
-            this.btnOK.TabIndex = 3;
-            this.btnOK.Text = "OK";
-            this.btnOK.UseVisualStyleBackColor = true;
-            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            btnOK.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnOK.Location = new Point(166, 176);
+            btnOK.Name = "btnOK";
+            btnOK.Size = new Size(75, 23);
+            btnOK.TabIndex = 3;
+            btnOK.Text = "OK";
+            btnOK.UseVisualStyleBackColor = true;
+            btnOK.Click += btnOK_Click;
             // 
             // lblRange
             // 
-            this.lblRange.AutoSize = true;
-            this.lblRange.Location = new System.Drawing.Point(9, 9);
-            this.lblRange.Name = "lblRange";
-            this.lblRange.Size = new System.Drawing.Size(144, 15);
-            this.lblRange.TabIndex = 0;
-            this.lblRange.Text = "Range of integer numbers";
+            lblRange.AutoSize = true;
+            lblRange.Location = new Point(9, 9);
+            lblRange.Name = "lblRange";
+            lblRange.Size = new Size(144, 15);
+            lblRange.TabIndex = 0;
+            lblRange.Text = "Range of integer numbers";
             // 
             // txtRange
             // 
-            this.txtRange.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtRange.Location = new System.Drawing.Point(12, 27);
-            this.txtRange.Multiline = true;
-            this.txtRange.Name = "txtRange";
-            this.txtRange.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtRange.Size = new System.Drawing.Size(310, 128);
-            this.txtRange.TabIndex = 1;
+            txtRange.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtRange.Location = new Point(12, 27);
+            txtRange.Multiline = true;
+            txtRange.Name = "txtRange";
+            txtRange.ScrollBars = ScrollBars.Vertical;
+            txtRange.Size = new Size(310, 128);
+            txtRange.TabIndex = 1;
             // 
             // lblExample
             // 
-            this.lblExample.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.lblExample.AutoSize = true;
-            this.lblExample.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.lblExample.Location = new System.Drawing.Point(9, 158);
-            this.lblExample.Name = "lblExample";
-            this.lblExample.Size = new System.Drawing.Size(119, 15);
-            this.lblExample.TabIndex = 2;
-            this.lblExample.Text = "For example: 1 - 5, 10";
+            lblExample.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            lblExample.AutoSize = true;
+            lblExample.ForeColor = SystemColors.GrayText;
+            lblExample.Location = new Point(9, 158);
+            lblExample.Name = "lblExample";
+            lblExample.Size = new Size(119, 15);
+            lblExample.TabIndex = 2;
+            lblExample.Text = "For example: 1 - 5, 10";
             // 
             // FrmRangeEdit
             // 
-            this.AcceptButton = this.btnOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(334, 211);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnOK);
-            this.Controls.Add(this.lblExample);
-            this.Controls.Add(this.txtRange);
-            this.Controls.Add(this.lblRange);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(300, 200);
-            this.Name = "FrmRangeEdit";
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Edit Range";
-            this.Load += new System.EventHandler(this.FrmRangeEdit_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AcceptButton = btnOK;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = btnCancel;
+            ClientSize = new Size(334, 211);
+            Controls.Add(btnCancel);
+            Controls.Add(btnOK);
+            Controls.Add(lblExample);
+            Controls.Add(txtRange);
+            Controls.Add(lblRange);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            MinimumSize = new Size(300, 200);
+            Name = "FrmRangeEdit";
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "Edit Range";
+            Load += FrmRangeEdit_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
