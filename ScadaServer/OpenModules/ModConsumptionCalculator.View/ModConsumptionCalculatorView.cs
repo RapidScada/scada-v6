b16b0478e1f793705f84d09b1302f2003a3f43a6
2mik <m_shir@mail.ru>
2024-01-22 18:35:13 +0300

    ModConsumptionCalculator: module structure

---
+++ ScadaServer/OpenModules/ModConsumptionCalculator.View/ModConsumptionCalculatorView.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Lang;
+
+namespace Scada.Server.Modules.ModConsumptionCalculator.View
+{
+    /// <summary>
+    /// Implements the server module user interface.
+    /// <para>Реализует пользовательский интерфейс серверного модуля.</para>
+    /// </summary>
+    public class ModConsumptionCalculatorView : ModuleView
+    {
+        /// <summary>
+        /// Gets the module name.
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return Locale.IsRussian ?
+                    "Калькулятор потребления" :
+                    "Consumption Calculator";
+            }
+        }
+
+        /// <summary>
+        /// Gets the module description.
+        /// </summary>
+        public override string Descr
+        {
+            get
+            {
+                return Locale.IsRussian ?
+                    "Модуль рассчитывает изменения значений каналов с заданной периодичностью." :
+                    "The module calculates changes of channel values with a specified period.";
+            }
+        }
+    }
+}
