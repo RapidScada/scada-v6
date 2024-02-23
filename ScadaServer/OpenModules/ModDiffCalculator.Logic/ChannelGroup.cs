8054853dcf258c64f84fcf80cc8ae88930fb6ace
2mik <m_shir@mail.ru>
2024-02-23 12:50:20 +0300

    ModDiffCalculator: logic structure

---
+++ ScadaServer/OpenModules/ModDiffCalculator.Logic/ChannelGroup.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Lang;
+using Scada.Server.Modules.ModDiffCalculator.Config;
+
+namespace Scada.Server.Modules.ModDiffCalculator.Logic
+{
+    /// <summary>
+    /// Represents a group of channels.
+    /// <para>Представляет группу каналов.</para>
+    /// </summary>
+    internal class ChannelGroup
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public ChannelGroup(GroupConfig groupConfig)
+        {
+            GroupConfig = groupConfig ?? throw new ArgumentNullException(nameof(groupConfig));
+            SrcCnlNums = groupConfig.Items.Select(i => i.SrcCnlNum).ToArray();
+            DestCnlNums = groupConfig.Items.Select(i => i.DestCnlNum).ToArray();
+        }
+
+
+        /// <summary>
+        /// Gets the group configuration.
+        /// </summary>
+        public GroupConfig GroupConfig { get; }
+
+        /// <summary>
+        /// Gets the group name.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return string.IsNullOrEmpty(GroupConfig.Name)
+                    ? (Locale.IsRussian ? "Безымянная" : "Unnamed")
+                    : GroupConfig.Name;
+            }
+        }
+
+        /// <summary>
+        /// Gets the source channel numbers.
+        /// </summary>
+        public int[] SrcCnlNums { get; }
+
+        /// <summary>
+        /// Gets the destination channel numbers corresponding to the source channels.
+        /// </summary>
+        public int[] DestCnlNums { get; }
+
+
+        /// <summary>
+        /// Checks if the specified time is the time to calculate differences.
+        /// </summary>
+        public bool IsTimeToCalculate(DateTime utcNow, out DateTime timestamp1, out DateTime timestamp2)
+        {
+            timestamp1 = DateTime.MinValue;
+            timestamp2 = DateTime.MinValue;
+            return false;
+        }
+    }
+}
--- ScadaServer/OpenModules/ModDiffCalculator.Logic/ModDiffCalculatorLogic.cs
+++ ScadaServer/OpenModules/ModDiffCalculator.Logic/ModDiffCalculatorLogic.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright (c) Rapid Software LLC. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Scada.Data.Const;
+using Scada.Data.Models;
 using Scada.Lang;
 using Scada.Log;
+using Scada.Protocol;
 using Scada.Server.Lang;
 using Scada.Server.Modules.ModDiffCalculator.Config;
 
@@ -19,8 +22,9 @@ namespace Scada.Server.Modules.ModDiffCalculator.Logic
         /// </summary>
         private const string LogFileName = ModuleUtils.ModuleCode + ".log";
 
-        private readonly LogFile moduleLog;         // the module log
-        private readonly ModuleConfig moduleConfig; // the module configuration
+        private readonly LogFile moduleLog;                // the module log
+        private readonly ModuleConfig moduleConfig;        // the module configuration
+        private readonly List<ChannelGroup> channelGroups; // the processed channel groups
 
         private Thread moduleThread;      // the working thread of the module
         private volatile bool terminated; // necessary to stop the thread
@@ -38,7 +42,8 @@ namespace Scada.Server.Modules.ModDiffCalculator.Logic
                 CapacityMB = serverContext.AppConfig.GeneralOptions.MaxLogSize
             };
             moduleConfig = new ModuleConfig();
-        
+            channelGroups = [];
+
             moduleThread = null;
             terminated = false;
         }
@@ -51,15 +56,82 @@ namespace Scada.Server.Modules.ModDiffCalculator.Logic
 
 
         /// <summary>
+        /// Initializes channel groups according to the module configuration.
+        /// </summary>
+        private bool InitChannelGroups(out string errMsg)
+        {
+            foreach (GroupConfig groupConfig in moduleConfig.Groups)
+            {
+                if (groupConfig.Active)
+                {
+                    ChannelGroup channelGroup = new(groupConfig);
+                    channelGroups.Add(channelGroup);
+                }
+            }
+
+            if (channelGroups.Count > 0)
+            {
+                errMsg = "";
+                return true;
+            }
+            else
+            {
+                errMsg = Locale.IsRussian ?
+                    "Отсутствуют активные группы каналов." :
+                    "Active channel groups missing.";
+                return false;
+            }
+        }
+
+        /// <summary>
         /// Performs calculations in a separate thread.
         /// </summary>
         private void Execute()
         {
             while (!terminated)
             {
-
-
+                DateTime utcNow = DateTime.UtcNow;
+                channelGroups.ForEach(group => ProcessChannelGroup(group, utcNow));
                 Thread.Sleep(ScadaUtils.ThreadDelay);
+            }
+        }
+
+        /// <summary>
+        /// Performs calculations for the specified channel group, if necessary.
+        /// </summary>
+        private void ProcessChannelGroup(ChannelGroup group, DateTime utcNow)
+        {
+            try
+            {
+                if (group.IsTimeToCalculate(utcNow, out DateTime timestamp1, out DateTime timestamp2))
+                {
+                    int archiveBit = 0;
+                    Slice srcSlice1 = ServerContext.GetSlice(archiveBit, timestamp1, group.SrcCnlNums);
+                    Slice srcSlice2 = ServerContext.GetSlice(archiveBit, timestamp2, group.SrcCnlNums);
+                    Slice destSlice = new(timestamp2, group.DestCnlNums);
+
+                    for (int i = 0; i < srcSlice1.Length; i++)
+                    {
+                        CnlData cnlData1 = srcSlice1.CnlData[i];
+                        CnlData cnlData2 = srcSlice2.CnlData[i];
+
+                        if (cnlData1.IsDefined && cnlData2.IsDefined)
+                        {
+                            destSlice.CnlData[i] = new CnlData(
+                                cnlData2.Val - cnlData1.Val,
+                                CnlStatusID.Defined);
+                        }
+                    }
+
+                    int archiveMask = 0;
+                    ServerContext.WriteHistoricalData(archiveMask, destSlice, WriteDataFlags.Default);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.WriteError(ex, Locale.IsRussian ?
+                    "Ошибка при обработке группы \"{0}\"" :
+                    "Error processing \"{0}\" group", group.Name);
             }
         }
 
@@ -74,7 +146,8 @@ namespace Scada.Server.Modules.ModDiffCalculator.Logic
             moduleLog.WriteAction(ServerPhrases.StartModule, Code, Version);
 
             // load configuration
-            if (moduleConfig.Load(Storage, ModuleConfig.DefaultFileName, out string errMsg))
+            if (moduleConfig.Load(Storage, ModuleConfig.DefaultFileName, out string errMsg) &&
+                InitChannelGroups(out errMsg))
             {
                 // start module thread
                 moduleThread = new Thread(Execute);
