fe1377d7e5cdaa8fa37b204bcffac4e1453839bf
2mik <m_shir@mail.ru>
2024-02-27 13:08:12 +0300

    ScadaCommon: channel number editor

---
+++ ScadaCommon/ScadaCommon/ComponentModel/IConfigDatasetAccessor.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Data.Models;
+
+namespace Scada.ComponentModel
+{
+    /// <summary>
+    /// Provides access to the configuration database.
+    /// <para>Предоставляет доступ к базе конфигурации.</para>
+    /// </summary>
+    public interface IConfigDatasetAccessor
+    {
+        /// <summary>
+        /// Gets the configuration database.
+        /// </summary>
+        ConfigDataset ConfigDataset { get; }
+    }
+}
