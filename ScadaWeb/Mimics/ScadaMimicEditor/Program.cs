f9da24cbcbfda7688964730dd94fdb166210bb66
2mik <m_shir@mail.ru>
2024-05-07 16:21:58 +0300

    Initial commit of Mimics

---
+++ ScadaWeb/Mimics/ScadaMimicEditor/Program.cs
@@ -0,0 +1,15 @@
+namespace ScadaMimicEditor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
