83996821ad1379c889b6c91e681c7138e1270203
2mik <m_shir@mail.ru>
2024-05-09 18:04:17 +0300

    Create Component.cs

---
+++ ScadaWeb/Mimics/PlgMimic.Common/MimicModel/Component.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Scada.Web.Plugins.PlgMimic.MimicModel
+{
+    /// <summary>
+    /// Represents a component of a mimic diagram.
+    /// <para>Представляет компонент мнемосхемы.</para>
+    /// </summary>
+    public class Component
+    {
+        /// <summary>
+        /// Gets or sets the component ID that is unique within the mimic.
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
