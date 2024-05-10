54a72bc34ffcc0ac3b77e513e20aa5137a3d0fa5
2mik <m_shir@mail.ru>
2024-05-10 15:31:37 +0300

    Mimics: add view project

---
+++ ScadaWeb/Mimics/PlgMimic/Controllers/MimicApiController.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc;
+using Scada.Web.Plugins.PlgMimic.MimicModel;
+
+namespace Scada.Web.Plugins.PlgMimic.Controllers
+{
+    /// <summary>
+    /// Represents the mimic web API.
+    /// <para>Представляет веб-API мнемосхем.</para>
+    /// </summary>
+    [ApiController]
+    [Route("Api/Mimic/[action]")]
+    public class MimicApiController : ControllerBase
+    {
+        public Component GetComponent()
+        {
+            Component component = new()
+            {
+                ID = 1,
+                Name = "Test"
+            };
+
+            return component;
+        }
+    }
+}
