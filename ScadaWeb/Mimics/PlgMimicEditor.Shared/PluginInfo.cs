608ce7171f46eae5ccc7107a4ce063e73747bfd7
2mik <m_shir@mail.ru>
2024-05-15 13:11:49 +0300

    Add PlgMimicEditor projects

---
+++ ScadaWeb/Mimics/PlgMimicEditor.Shared/PluginInfo.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Lang;
+
+namespace Scada.Web.Plugins.PlgMimicEditor
+{
+    /// <summary>
+    /// Represents information about a plugin.
+    /// <para>Представляет информацию о плагине.</para>
+    /// </summary>
+    internal class PluginInfo : LibraryInfo
+    {
+        /// <summary>
+        /// Gets the plugin code.
+        /// </summary>
+        public override string Code => "PlgMimicEditor";
+
+        /// <summary>
+        /// Gets the plugin name.
+        /// </summary>
+        public override string Name => Locale.IsRussian ?
+            "Редактор мнемосхем" :
+            "Mimic Editor";
+
+        /// <summary>
+        /// Gets the plugin description.
+        /// </summary>
+        public override string Descr => Locale.IsRussian ?
+            "Плагин обеспечивает редактирование мнемосхем." :
+            "The plugin provides editing mimic diagrams.";
+    }
+}
---
+++ ScadaWeb/Mimics/PlgMimicEditor/Areas/MimicEditor/Pages/Page1.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PlgMimicEditor.MyFeature.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
