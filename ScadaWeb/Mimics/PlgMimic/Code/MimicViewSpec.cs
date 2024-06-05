7836ef35731d135add1136f2b1aa19330d859eb1
2mik <m_shir@mail.ru>
2024-06-05 18:23:53 +0300

    Mimics: view

---
+++ ScadaWeb/Mimics/PlgMimic/Code/MimicViewSpec.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Scada.Web.Plugins.PlgMimic.Code
+{
+    /// <summary>
+    /// Represents a scheme view specification.
+    /// <para>Представляет спецификацию представления схем.</para>
+    /// </summary>
+    public class MimicViewSpec : ViewSpec
+    {
+        /// <summary>
+        /// Gets the view type code.
+        /// </summary>
+        public override string TypeCode => nameof(MimicView);
+
+        /// <summary>
+        /// Gets the extension of view files.
+        /// </summary>
+        public override string FileExtension => "mim";
+
+        /// <summary>
+        /// Gets the view icon URL.
+        /// </summary>
+        public override string IconUrl => "~/plugins/Mimic/images/mimic-icon.png";
+
+        /// <summary>
+        /// Gets the view type.
+        /// </summary>
+        public override Type ViewType => typeof(TypeCode);
+
+
+        /// <summary>
+        /// Gets the view frame URL.
+        /// </summary>
+        public override string GetFrameUrl(int viewID) => "~/Mimic/MimicView/" + viewID;
+    }
+}
--- ScadaWeb/Mimics/PlgMimic/PlgMimicLogic.cs
+++ ScadaWeb/Mimics/PlgMimic/PlgMimicLogic.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Rapid Software LLC. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Scada.Web.Plugins.PlgMimic.Code;
 using Scada.Web.Services;
 
 namespace Scada.Web.Plugins.PlgMimic
@@ -19,5 +20,10 @@ namespace Scada.Web.Plugins.PlgMimic
         {
             Info = new MimicPluginInfo();
         }
+
+        /// <summary>
+        /// Gets the view specifications.
+        /// </summary>
+        public override ICollection<ViewSpec> ViewSpecs => [new MimicViewSpec()];
     }
 }
