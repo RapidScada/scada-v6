3686dc8da1f6037176592b7ed99e889758b559d7
2mik <m_shir@mail.ru>
2024-06-21 17:14:08 +0300

    Update mimic-render.js

--- ScadaWeb/Mimics/PlgMimic/wwwroot/plugins/Mimic/js/mimic-render.js
+++ ScadaWeb/Mimics/PlgMimic/wwwroot/plugins/Mimic/js/mimic-render.js
@@ -15,22 +15,66 @@ rs.mimic.Renderer = class {
 
 // Represents a mimic renderer.
 rs.mimic.MimicRenderer = class extends rs.mimic.Renderer {
+    createDom(component, renderContext) {
+        component.dom = $("<div class='mimic'></div>");
+        this.updateDom(component, renderContext);
+    }
+
+    updateDom(component, renderContext) {
+        if (component.dom instanceof jQuery) {
+            let mimicWidth = component.document.size.width;
+            let mimicHeight = component.document.size.height;
+            let mimicElem = component.dom.first();
+
+            mimicElem.css({
+                "width": mimicWidth,
+                "height": mimicHeight
+            });
+        }
+    }
 }
 
 // Represents a component renderer.
 rs.mimic.ComponentRenderer = class extends rs.mimic.Renderer {
+    createDom(component, renderContext) {
+        component.dom = $("<div id='" + component.id + "' class='comp'></div>");
+    }
 }
 
 // Represents a text component renderer.
 rs.mimic.TextRenderer = class extends rs.mimic.ComponentRenderer {
+    createDom(component, renderContext) {
+        super.createDom(component, renderContext);
+    }
 }
 
 // Represents a picture component renderer.
 rs.mimic.PictureRenderer = class extends rs.mimic.ComponentRenderer {
+    createDom(component, renderContext) {
+        super.createDom(component, renderContext);
+    }
 }
 
 // Represents a panel component renderer.
 rs.mimic.PanelRenderer = class extends rs.mimic.ComponentRenderer {
+    createDom(component, renderContext) {
+        super.createDom(component, renderContext);
+        this.updateDom(component, renderContext);
+    }
+
+    updateDom(component, renderContext) {
+        if (component.dom instanceof jQuery) {
+            let panelElem = component.dom.first();
+            let props = component.properties;
+
+            panelElem.css({
+                "left": props.location.x,
+                "top": props.location.y,
+                "width": props.size.width,
+                "height": props.size.height
+            });
+        }
+    }
 }
 
 // Encapsulates information about a rendering operation.
