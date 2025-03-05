d6758bd3d06fbab0055bb49057a2ecc037ab1e72
2mik <m_shir@mail.ru>
2025-03-05 13:46:43 +0300

    ScadaAdmin, ExtTableEditor: update extension API

---
+++ ScadaAdmin/ScadaAdmin/ScadaAdminCommon/Extensions/OpenFileResult.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Scada.Admin.Extensions
+{
+    /// <summary>
+    /// Represents a result of opening a file with an extension.
+    /// <para>Представляет результат открытия файла расширением.</para>
+    /// </summary>
+    public class OpenFileResult
+    {
+        /// <summary>
+        /// Gets a value indicating whether the result is successful.
+        /// </summary>
+        public bool Ok { get; init; } = false;
+
+        /// <summary>
+        /// Gets the form to edit the open file.
+        /// </summary>
+        public Form EditorForm { get; init; } = null;
+    }
+}
