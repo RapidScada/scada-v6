2f8c3dc0d2756b82f8f7adb7cb7eafd609c12305
2mik <m_shir@mail.ru>
2025-04-07 17:22:53 +0300

    Create struct-tree.js

---
+++ ScadaWeb/Mimics/PlgMimicEditor/wwwroot/plugins/MimicEditor/js/struct-tree.js
@@ -0,0 +1,21 @@
+﻿// Contains classes: StructTree
+// Depends on jquery
+
+// Represents a component for displaying mimic structure.
+class StructTree {
+    structElem;
+    mimic;
+
+    constructor(elemID, mimic) {
+        this.structElem = $("#" + elemID);
+        this.mimic = mimic;
+    }
+
+    prepare() {
+
+    }
+
+    addComponent(component) {
+
+    }
+}
