1355a983545346e2ae8ec263688dce5c02e05a84
2mik <m_shir@mail.ru>
2026-02-13 12:34:04 +0300

    Update ScadaUtils.cs

--- ScadaCommon/ScadaCommon/ScadaUtils.cs
+++ ScadaCommon/ScadaCommon/ScadaUtils.cs
@@ -311,21 +311,16 @@ namespace Scada
         /// </summary>
         public static T SafeClone<T>(this T obj)
         {
-            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            if (obj == null)
+                return default;
+
+            XmlSerializer serializer = new XmlSerializer(obj.GetType());
 
             using (MemoryStream stream = new MemoryStream())
             {
-                using (XmlWriter writer = XmlWriter.Create(stream))
-                {
-                    serializer.Serialize(writer, obj);
-                }
-
+                serializer.Serialize(stream, obj);
                 stream.Position = 0;
-
-                using (XmlReader reader = XmlReader.Create(stream))
-                {
-                    return (T)serializer.Deserialize(reader);
-                }
+                return (T)serializer.Deserialize(stream);
             }
         }
 
