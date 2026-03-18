b92a18c281a29c5393abb04978574ec6ae6c0199
2mik <m_shir@mail.ru>
2026-03-18 18:04:07 +0300

    PlgMimicEditor: enable scripts in edit mode

--- ScadaWeb/Mimics/PlgMimicEditor/wwwroot/plugins/MimicEditor/js/mimic-edit.js
+++ ScadaWeb/Mimics/PlgMimicEditor/wwwroot/plugins/MimicEditor/js/mimic-edit.js
@@ -317,6 +317,7 @@ async function loadMimic() {
     // load
     showSpinner();
     let result = await mimic.load(getLoaderUrl(), mimicKey);
+    mimic.initCustomScripts();
 
     if (!result.ok) {
         showToast(phrases.loadMimicError, MessageType.ERROR);
