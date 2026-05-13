42ffc7d6e7b197e38f395095b897c6bb409b4a95
2mik <m_shir@mail.ru>
2026-05-13 13:24:58 +0300

    DrvRsClient: driver structure

---
+++ ScadaComm/OpenDrivers2/DrvRsClient.Logic/DevRsClientLogic.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Comm.Config;
+using Scada.Comm.Devices;
+
+namespace Scada.Comm.Drivers.DrvRsClient.Logic
+{
+    /// <summary>
+    /// Implements the device logic.
+    /// <para>Реализует логику устройства.</para>
+    /// </summary>
+    internal class DevRsClientLogic : DeviceLogic
+    {
+        public DevRsClientLogic(ICommContext commContext, ILineContext lineContext, DeviceConfig deviceConfig)
+            : base(commContext, lineContext, deviceConfig)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Performs actions when starting a communication line.
+        /// </summary>
+        public override void OnCommLineStart()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes the device tags.
+        /// </summary>
+        public override void InitDeviceTags()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes the device data.
+        /// </summary>
+        public override void InitDeviceData()
+        {
+
+        }
+
+        /// <summary>
+        /// Performs a communication session.
+        /// </summary>
+        public override void Session()
+        {
+
+        }
+    }
+}
---
+++ ScadaComm/OpenDrivers2/DrvRsClient.View/DevRsClientView.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Comm.Config;
+using Scada.Comm.Devices;
+
+namespace Scada.Comm.Drivers.DrvRsClient.View
+{
+    /// <summary>
+    /// Implements the device user interface.
+    /// <para>Реализует пользовательский интерфейс устройства.</para>
+    /// </summary>
+    internal class DevRsClientView : DeviceView
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public DevRsClientView(DriverView parentView, LineConfig lineConfig, DeviceConfig deviceConfig)
+            : base(parentView, lineConfig, deviceConfig)
+        {
+            CanShowProperties = true;
+        }
+
+
+        /// <summary>
+        /// Shows a modal dialog box for editing device properties.
+        /// </summary>
+        public override bool ShowProperties()
+        {
+            //DriverUtils.ConfigDataset = ConfigDataset;
+            //new FrmModuleConfig(new RsClientConfigProvider(AppDirs.ConfigDir, DeviceNum)).ShowDialog();
+            return false;
+        }
+
+        /// <summary>
+        /// Gets the default polling options for the device.
+        /// </summary>
+        public override PollingOptions GetPollingOptions()
+        {
+            return PollingOptions.CreateWithDefaultDelay();
+        }
+    }
+}
