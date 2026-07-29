2eda6f7ddf59805c2e4a3f4cca7546200fc31319
2mik <9930867+2mik@users.noreply.github.com>
2026-07-29 16:44:10 +0300

    DrvSmsParser: dev

---
+++ ScadaComm/OpenDrivers2/DrvSmsParser.Shared/Config/DeviceTemplate.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Config;
+
+namespace DrvSmsParser.Shared.Config
+{
+    /// <summary>
+    /// Represents a device template.
+    /// <para>Представляет шаблон устройства.</para>
+    /// </summary>
+    internal class DeviceTemplate : ConfigBase
+    {
+    }
+}
