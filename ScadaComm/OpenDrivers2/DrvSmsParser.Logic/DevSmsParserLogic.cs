9e65f0470be9e6a3ffdbfff456a46d787e2a9527
2mik <9930867+2mik@users.noreply.github.com>
2026-07-30 18:07:54 +0300

    DrvSmsParser: dev

--- ScadaComm/OpenDrivers2/DrvSmsParser.Logic/DevSmsParserLogic.cs
+++ ScadaComm/OpenDrivers2/DrvSmsParser.Logic/DevSmsParserLogic.cs
@@ -1,10 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Comm.Config;
+using Scada.Comm.Devices;
+using Scada.Config;
 
 namespace Scada.Comm.Drivers.DrvSmsParser.Logic
 {
-    internal class DevSmsParserLogic
+    /// <summary>
+    /// Implements the device logic.
+    /// <para>Реализует логику устройства.</para>
+    /// </summary>
+    internal class DevSmsParserLogic : DeviceLogic
     {
+        private TimeSpan dataLifetime;       // specifies when tag values should be invalidated
+        private bool useDataLifetime;        // indicates that lifetime is used
+        private DateTime[] updateTimestamps; // the update timestamps by device tag
+
+
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        public DevSmsParserLogic(ICommContext commContext, ILineContext lineContext, DeviceConfig deviceConfig)
+            : base(commContext, lineContext, deviceConfig)
+        {
+            dataLifetime = TimeSpan.Zero;
+            useDataLifetime = false;
+            updateTimestamps = null;
+
+            ConnectionRequired = false;
+        }
+
+        /// <summary>
+        /// Performs actions when starting a communication line.
+        /// </summary>
+        public override void OnCommLineStart()
+        {
+            dataLifetime = TimeSpan.FromSeconds(LineContext.LineConfig.CustomOptions.GetValueAsInt("DataLifetime"));
+            useDataLifetime = dataLifetime > TimeSpan.Zero;
+        }
+        
+        /// <summary>
+        /// Initializes the device tags.
+        /// </summary>
+        public override void InitDeviceTags()
+        {
+
+        }
+
+        /// <summary>
+        /// Performs a communication session.
+        /// </summary>
+        public override void Session()
+        {
+
+        }
     }
 }
