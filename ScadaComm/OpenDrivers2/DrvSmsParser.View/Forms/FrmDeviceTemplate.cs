33e8aec3dd951dbdf1640e1cf4d930e5881af025
2mik <9930867+2mik@users.noreply.github.com>
2026-08-31 17:31:48 +0300

    DrvSmsParser: add forms

---
+++ ScadaComm/OpenDrivers2/DrvSmsParser.View/Forms/FrmDeviceTemplate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Scada.Comm.Drivers.DrvSmsParser.View.Forms
+{
+    public partial class FrmDeviceTemplate : Form
+    {
+        public FrmDeviceTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
